cc47b3a12c08242037e1e799b71a3c217d659feb
Jonathan McPherson <jonathan@rstudio.com>
2022-01-12 12:51:42 -0800

    implement connection file parsing

---
+++ src/connection_file.rs
@@ -0,0 +1,41 @@
+/*
+ * connection_file.rs
+ *
+ * Copyright (C) 2022 by RStudio, PBC
+ *
+ */
+
+use serde::Deserialize;
+use std::error::Error;
+use std::fs::File;
+use std::io::BufReader;
+use std::path::Path;
+
+#[derive(Deserialize)]
+pub struct ConnectionFile {
+    // ZeroMQ ports
+    control_port: u16,
+    shell_port: u16,
+    stdin_port: u16,
+    iopub_port: u16,
+    hb_port: u16,
+
+    // TODO: enum? "tcp"
+    transport: String,
+    // TODO: enum? "hmac-sha256"
+    signature_scheme: String,
+
+    ip: String,
+    key: String,
+}
+
+impl ConnectionFile {
+    /// Create a ConnectionFile by parsing the contents of a connection file.
+    pub fn from_file<P: AsRef<Path>>(connection_file: P) -> Result<ConnectionFile, Box<dyn Error>> {
+        let file = File::open(connection_file)?;
+        let reader = BufReader::new(file);
+        let control = serde_json::from_reader(reader)?;
+
+        Ok(control)
+    }
+}
