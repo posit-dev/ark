7b737ca59f4f45a0f670159153fb9ac60a54bb67
Jonathan McPherson <jonathan@rstudio.com>
2022-01-11 17:04:42 -0800

    basic arg matching

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,30 @@
  */
 
 fn main() {
-    println!("Hello, world!");
+    println!("Amalthea: An R kernel for Myriac and Jupyter.");
+
+    // Get an iterator over all the command-line arguments
+    let mut argv = std::env::args();
+
+    // Skip the first "argument" as it's the path/name to this executable
+    argv.next();
+
+    // Process remaining arguments
+    match argv.next() {
+        Some(arg) => {
+            match arg.as_str() {
+                "--control_file" => {
+                    // TODO: handle missing control file
+                    println!("Loading control file {}", argv.next().unwrap())
+                },
+                other => {
+                    eprintln!("Argument '{}' unknown", other);
+                }
+            }
+        }
+        None => {
+            println!("Usage: amalthea --control_file /path/to/file");
+        }
+    } 
 }
 
