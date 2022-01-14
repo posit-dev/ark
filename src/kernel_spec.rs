b1439833620bc716ea69e613d179c14f1bd5e7cd
Jonathan McPherson <jonathan@rstudio.com>
2022-01-14 11:32:27 -0800

    XDG_DATA_DIR impl

--- src/kernel_spec.rs
+++ src/kernel_spec.rs
@@ -5,23 +5,34 @@
  *
  */
 
- /// From the Jupyter documentation for [Kernel Specs](https://jupyter-client.readthedocs.io/en/stable/kernels.html#kernel-specs).
- pub struct KernelSpec {
+ use crate::kernel_dirs;
 
-    /// List of command line arguments to be used to start the kernel
-    pub argv: Vec<String>;
+/// From the Jupyter documentation for [Kernel Specs](https://jupyter-client.readthedocs.io/en/stable/kernels.html#kernel-specs).
+pub struct KernelSpec {
 
-    // The kernel name as it should be displayed in the UI
-    pub display_name: String;
+   /// List of command line arguments to be used to start the kernel
+   pub argv: Vec<String>;
 
-    // The kernel's language
-    pub language: String;
- }
+   // The kernel name as it should be displayed in the UI
+   pub display_name: String;
+
+   // The kernel's language
+   pub language: String;
+}
 
 
 impl KernelSpec {
     pub fn install(&self) -> Result<()> {
+        if let Some(kernel_dir) = kernel_dirs::jupyter_kernel_path() {
+            install_to(kernel_dir.join("amalthea"))
+            Ok(())
+        }
+        Err(concat!("Could not locate Jupyter installation directory. "
+            "Check XDG_DATA_PATH environment variables and set JUPYTER_PATH if necessary."))
+    }
 
+    fn install_to(&self, path: PathBuf) -> () {
+        ()
     }
 }
 
