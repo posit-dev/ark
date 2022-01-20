1be32a455ec7e1689319681648a712263c91704a
Jonathan McPherson <jonathan@rstudio.com>
2022-01-20 15:19:07 -0800

    message parsing improvements (WIP)

--- src/wire/message.rs
+++ src/wire/message.rs
@@ -6,7 +6,9 @@
  */
 
 use crate::wire::header::JupyterHeader;
+use hmac::Hmac;
 use serde::Serialize;
+use std::fmt;
 
 /// This delimiter separates the ZeroMQ socket identities (IDS) from the message
 /// body payload (MSG).
@@ -31,22 +33,41 @@ pub struct JupyterMessage<T> {
     pub buffers: (),
 }
 
-impl JupyterMessage<T> {
-    /// Parse a Jupyter message from an array of buffers (from a ZeroMQ message)
-    pub fn from_buffers(
-        mut bufs: Vec<Vec<u8>>,
-    ) -> Result<JupyterMessage, Box<dyn std::error::Error>> {
-        let iter = bufs.iter();
-        if let Some(pos) = iter.position(|buf| &buf[..] == MSG_DELIM) {
-            return from_msg_bufs(bufs[pos+1..]);
+#[derive(Debug)]
+pub enum MessageError {
+    MissingDelimiter,
+}
+
+impl fmt::Display for MessageError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            MessageError::MissingDelimiter => {
+                write!(
+                    f,
+                    "ZeroMQ message did not include expected <IDS|MSG> delimiter"
+                )
+            }
         }
-        // TODO: need real error
-        return Err()
+    }
+}
+
+impl<T> JupyterMessage<T> {
+    /// Parse a Jupyter message from an array of buffers (from a ZeroMQ message)
+    pub fn from_buffers(bufs: Vec<Vec<u8>>) -> Result<JupyterMessage<T>, MessageError> {
+        let mut iter = bufs.iter();
+
+        // Find the position of the <IDS|MSG> delimiter in the message, which
+        // separates the socket identities (IDS) from the body of the message.
+        if let Some(pos) = iter.position(|buf| &buf[..] == MSG_DELIM) {
+            return JupyterMessage::from_msg_bufs(bufs[pos + 1..].to_vec());
+        }
+
+        // No delimiter found.
+        return Err(MessageError::MissingDelimiter);
     }
 
-    fn from_msg_bufs(mut bufs: Vec<Vec<u8>>) -> Result<JupyterMessage, Box<dyn std::error::Error>> {
-        
-    }
-
+    fn from_msg_bufs(mut bufs: Vec<Vec<u8>>) -> Result<JupyterMessage<T>, MessageError> {
+        // TODO
+        Err(MessageError::MissingDelimiter)
     }
 }
