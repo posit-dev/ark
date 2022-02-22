e53d8ff0e82e212cdb931c91fc95d8a247d8b493
Jonathan McPherson <jonathan@rstudio.com>
2022-02-22 11:42:21 -0800

    begin factor into lib/bin components

---
+++ amalthea/src/lib.rs
@@ -0,0 +1,17 @@
+/*
+ * lib.rs
+ *
+ * Copyright (C) 2022 by RStudio, PBC
+ *
+ */
+
+mod connection_file;
+mod error;
+mod kernel;
+mod kernel_dirs;
+mod kernel_spec;
+mod language;
+mod session;
+mod socket;
+mod wire;
+
