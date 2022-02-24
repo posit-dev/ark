9ec28ecf1105d65330c5f6ffd6d16215b74dd781
Jonathan McPherson <jonathan@rstudio.com>
2022-02-24 15:54:57 -0800

    use iopub-specific message enum (WIP)

--- amalthea/src/socket/iopub.rs
+++ amalthea/src/socket/iopub.rs
@@ -7,8 +7,11 @@
 
 use crate::error::Error;
 use crate::socket::socket::Socket;
+use crate::wire::execute_error::ExecuteError;
+use crate::wire::execute_input::ExecuteInput;
+use crate::wire::execute_result::ExecuteResult;
+use crate::wire::header::JupyterHeader;
 use crate::wire::jupyter_message::JupyterMessage;
-use crate::wire::jupyter_message::Message;
 use crate::wire::jupyter_message::ProtocolMessage;
 use crate::wire::status::ExecutionState;
 use crate::wire::status::KernelStatus;
@@ -16,15 +19,30 @@ use log::{trace, warn};
 use std::sync::mpsc::Receiver;
 
 pub struct IOPub {
+    /// The underlying IOPub socket
     socket: Socket,
-    receiver: Receiver<Message>,
+
+    /// A channel that receives IOPub messages from other threads
+    receiver: Receiver<IOPubMessage>,
+
+    /// The current message context; attached to outgoing messages to pair
+    /// outputs with the message that caused them.
+    context: Option<JupyterHeader>,
+}
+
+pub enum IOPubMessage {
+    Status(JupyterHeader, KernelStatus),
+    ExecuteResult(ExecuteResult),
+    ExecuteError(ExecuteError),
+    ExecuteInput(ExecuteInput),
 }
 
 impl IOPub {
-    pub fn new(socket: Socket, receiver: Receiver<Message>) -> Self {
+    pub fn new(socket: Socket, receiver: Receiver<IOPubMessage>) -> Self {
         Self {
             socket: socket,
             receiver: receiver,
+            context: None,
         }
     }
 
@@ -45,18 +63,21 @@ impl IOPub {
         }
     }
 
-    fn process_message(&self, message: Message) -> Result<(), Error> {
+    fn process_message(&mut self, message: IOPubMessage) -> Result<(), Error> {
         match message {
-            Message::Status(msg) => self.send_message(msg),
-            Message::ExecuteResult(msg) => self.send_message(msg),
-            Message::ExecuteError(msg) => self.send_message(msg),
-            Message::ExecuteInput(msg) => self.send_message(msg),
-            _ => Err(Error::UnsupportedMessage(message, String::from("iopub"))),
+            IOPubMessage::Status(context, msg) => {
+                self.context = Some(context);
+                self.send_message(msg)
+            }
+            IOPubMessage::ExecuteResult(msg) => self.send_message(msg),
+            IOPubMessage::ExecuteError(msg) => self.send_message(msg),
+            IOPubMessage::ExecuteInput(msg) => self.send_message(msg),
         }
     }
 
-    fn send_message<T: ProtocolMessage>(&self, message: JupyterMessage<T>) -> Result<(), Error> {
-        message.send(&self.socket)
+    fn send_message<T: ProtocolMessage>(&self, content: T) -> Result<(), Error> {
+        let msg = JupyterMessage::<T>::create(content, self.context, &self.socket.session);
+        msg.send(&self.socket)
     }
 
     fn emit_state(&self, state: ExecutionState) {
