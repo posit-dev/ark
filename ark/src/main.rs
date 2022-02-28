2f0d12d4328fa5280903f47e8691fb00c609d2cc
Jonathan McPherson <jonathan@rstudio.com>
2022-02-28 14:12:17 -0800

    initialize R (not linking yet)

--- ark/src/main.rs
+++ ark/src/main.rs
@@ -5,6 +5,19 @@
  *
  */
 
+use libc::{c_char, c_int};
+use std::ffi::CString;
+
+#[link(name = "libR")]
+extern "C" {
+    fn Rf_initialize_R(ac: c_int, av: *const c_char) -> i32;
+    // static foo_global: c_int;
+}
+
 fn main() {
+    let args = CString::new("").unwrap();
+    unsafe {
+        Rf_initialize_R(0, args.as_ptr());
+    }
     println!("Hello, world.")
 }
