0777cb031f5ec524e472a3aa3deaf36da80d670b
Jonathan McPherson <jonathan@rstudio.com>
2022-03-01 15:01:21 -0800

    set main program flag

--- ark/src/main.rs
+++ ark/src/main.rs
@@ -12,12 +12,16 @@ use std::ffi::CString;
 extern "C" {
     // TODO: this is actually a vector of cstrings
     fn Rf_initialize_R(ac: c_int, av: *const c_char) -> i32;
-    // static foo_global: c_int;
+
+    /// Global indicating whether R is running as the main program (affects
+    /// R_CStackStart)
+    static mut R_running_as_main_program: c_int;
 }
 
 fn main() {
     let args = CString::new("").unwrap();
     unsafe {
+        R_running_as_main_program = 1;
         Rf_initialize_R(0, args.as_ptr());
     }
     println!("Hello, world.")
