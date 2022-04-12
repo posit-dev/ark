df22e2aa9d8d45fd2933bc196542702af78d6040
Jonathan McPherson <jonathan@rstudio.com>
2022-04-12 09:50:49 -0700

    use socket transport for lsp

--- ark/extension/src/extension.ts
+++ ark/extension/src/extension.ts
@@ -1,16 +1,44 @@
 import * as vscode from 'vscode';
+import * as path from 'path';
 
+import {
+	LanguageClient,
+	LanguageClientOptions,
+	ServerOptions,
+	TransportKind,
+	createClientSocketTransport
+} from 'vscode-languageclient/node';
+
+let client: LanguageClient;
 export function activate(context: vscode.ExtensionContext) {
 
 	console.log('Activating ARK language server extension');
 
 	let disposable = vscode.commands.registerCommand('ark.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World from ark!');
 	});
 
 	context.subscriptions.push(disposable);
+
+	let serverOptions = () => {
+		// TODO: port needs to be configurable or discoverable
+		return createClientSocketTransport(9276).then(transport => {
+			return transport.onConnected().then((protocol) => {
+				return {
+					reader: protocol[0],
+					writer: protocol[1]
+				};
+			});
+		});
+	};
+
+	let clientOptions: LanguageClientOptions = {
+		documentSelector: [{ scheme: 'file', language: 'R' }],
+	};
+
+	client = new LanguageClient('ark', 'ARK Language Server', serverOptions, clientOptions);
+
+	client.start();
 }
 
 export function deactivate() { }
