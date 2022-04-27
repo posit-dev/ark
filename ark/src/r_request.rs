4eb85dbc706d5d580f797b109473f07c842170da
Jonathan McPherson <jonathan@rstudio.com>
2022-04-27 13:13:09 -0700

    wait for execution to complete

--- ark/src/r_request.rs
+++ ark/src/r_request.rs
@@ -5,12 +5,14 @@
  *
  */
 
+use amalthea::wire::execute_reply::ExecuteReply;
 use amalthea::wire::execute_request::ExecuteRequest;
+use std::sync::mpsc::Sender;
 
 /// Represents requests to the primary R execution thread.
 pub enum RRequest {
     /// Fulfill an execution request from the front end
-    ExecuteCode(ExecuteRequest),
+    ExecuteCode(ExecuteRequest, Sender<ExecuteReply>),
 
     /// Shut down the R execution thread
     Shutdown(bool),
