55c83aab910aca9d5f6b98dab91deac53de90412
Jonathan McPherson <jonathan@rstudio.com>
2022-05-12 14:46:07 -0700

    run kernel on separate thread during test

--- amalthea/tests/client.rs
+++ amalthea/tests/client.rs
@@ -7,19 +7,32 @@
 
 use amalthea::kernel::Kernel;
 use amalthea::socket::iopub::IOPubMessage;
-use amalthea::wire::jupyter_message::{JupyterMessage, Message};
+use amalthea::wire::jupyter_message::Message;
 use amalthea::wire::kernel_info_request::KernelInfoRequest;
-use log::{debug, error, info};
-use std::io::stdin;
+use env_logger;
+use log::info;
 use std::sync::mpsc::sync_channel;
 use std::sync::{Arc, Mutex};
+use std::thread;
 
 mod control;
 mod frontend;
 mod shell;
 
+// One-time construction of the logging system.
+use std::sync::Once;
+static INIT: Once = Once::new();
+fn setup() {
+    INIT.call_once(|| {
+        env_logger::init();
+    });
+}
+
 #[test]
 fn test_kernel() {
+    // Set up test logger
+    setup();
+
     // This channel delivers execution status and other iopub messages from
     // other threads to the iopub thread
     let (iopub_sender, iopub_receiver) = sync_channel::<IOPubMessage>(10);
@@ -28,17 +41,20 @@ fn test_kernel() {
     let shell = Arc::new(Mutex::new(shell::Shell::new(shell_sender)));
     let control = Arc::new(Mutex::new(control::Control {}));
     let frontend = frontend::Frontend::new();
+    let connection_file = frontend.get_connection_file();
 
-    // Create and connect the kernel to the front end
-    let kernel = Kernel::new(frontend.get_connection_file()).unwrap();
-    kernel
-        .connect(shell, control, iopub_sender, iopub_receiver)
-        .unwrap();
+    // Create the thread that will run the Amalthea kernel
+    thread::spawn(move || {
+        let kernel = Kernel::new(connection_file).unwrap();
+        kernel
+            .connect(shell, control, iopub_sender, iopub_receiver)
+            .unwrap();
+    });
 
     // Ask the kernel for the kernel info
     frontend.send_shell(KernelInfoRequest {});
 
-    let reply = frontend.receive();
+    let reply = frontend.receive_shell();
     match reply {
         Message::KernelInfoReply(reply) => {
             info!("Kernel info: {:?}", reply);
--- amalthea/tests/frontend/mod.rs
+++ amalthea/tests/frontend/mod.rs
@@ -9,23 +9,20 @@ use amalthea::connection_file::ConnectionFile;
 use amalthea::session::Session;
 use amalthea::socket::socket::Socket;
 use amalthea::wire::jupyter_message::{JupyterMessage, Message, ProtocolMessage};
-use std::sync::mpsc::{channel, Receiver, Sender};
-use std::thread;
 
 pub struct Frontend {
+    pub control_socket: Socket,
+    pub shell_socket: Socket,
+    pub iopub_socket: Socket,
+    pub stdin_socket: Socket,
+    pub heartbeat_socket: Socket,
     session: Session,
-    receiver: Receiver<Message>,
     key: String,
     control_port: u16,
-    control_socket: Socket,
     shell_port: u16,
-    shell_socket: Socket,
     iopub_port: u16,
-    iopub_socket: Socket,
     stdin_port: u16,
-    stdin_socket: Socket,
     heartbeat_port: u16,
-    heartbeat_socket: Socket,
 }
 
 impl Frontend {
@@ -39,9 +36,6 @@ impl Frontend {
         // Create a new kernel session from the key
         let session = Session::create(key.clone()).unwrap();
 
-        // Create an MPSC channel for receiving kernel messages.
-        let (sender, receiver) = channel::<Message>();
-
         let ctx = zmq::Context::new();
 
         let control_port = portpicker::pick_unused_port().unwrap();
@@ -53,9 +47,6 @@ impl Frontend {
             format!("tcp://127.0.0.1:{}", control_port),
         )
         .unwrap();
-        let control_socket = control.clone();
-        let control_sender = sender.clone();
-        thread::spawn(move || Self::message_proxy_thread(control_socket, control_sender));
 
         let shell_port = portpicker::pick_unused_port().unwrap();
         let shell = Socket::new(
@@ -66,22 +57,16 @@ impl Frontend {
             format!("tcp://127.0.0.1:{}", shell_port),
         )
         .unwrap();
-        let shell_socket = shell.clone();
-        let shell_sender = sender.clone();
-        thread::spawn(move || Self::message_proxy_thread(shell_socket, shell_sender));
 
         let iopub_port = portpicker::pick_unused_port().unwrap();
         let iopub = Socket::new(
             session.clone(),
             ctx.clone(),
             String::from("IOPub"),
-            zmq::PUB,
+            zmq::SUB,
             format!("tcp://127.0.0.1:{}", iopub_port),
         )
         .unwrap();
-        let iopub_socket = iopub.clone();
-        let iopub_sender = sender.clone();
-        thread::spawn(move || Self::message_proxy_thread(iopub_socket, iopub_sender));
 
         let stdin_port = portpicker::pick_unused_port().unwrap();
         let stdin = Socket::new(
@@ -92,9 +77,6 @@ impl Frontend {
             format!("tcp://127.0.0.1:{}", stdin_port),
         )
         .unwrap();
-        let stdin_socket = stdin.clone();
-        let stdin_sender = sender.clone();
-        thread::spawn(move || Self::message_proxy_thread(stdin_socket, stdin_sender));
 
         let heartbeat_port = portpicker::pick_unused_port().unwrap();
         let heartbeat = Socket::new(
@@ -108,7 +90,6 @@ impl Frontend {
 
         Self {
             session,
-            receiver,
             key,
             control_port,
             control_socket: control,
@@ -123,15 +104,15 @@ impl Frontend {
         }
     }
 
-    /// Receives and returns the next message from the kernel (from any socket)
-    pub fn receive(&self) -> Message {
-        self.receiver.recv().unwrap()
-    }
-
     /// Sends a message on the Shell socket
     pub fn send_shell<T: ProtocolMessage>(&self, msg: T) {
         let message = JupyterMessage::create(msg, None, &self.session);
         message.send(&self.shell_socket).unwrap();
+    }
+
+    /// Receives a message from the Shell socket
+    pub fn receive_shell(&self) -> Message {
+        Message::read_from_socket(&self.shell_socket).unwrap()
     }
 
     pub fn get_connection_file(&self) -> ConnectionFile {
@@ -145,20 +126,6 @@ impl Frontend {
             signature_scheme: String::from("hmac-sha256"),
             ip: String::from("127.0.0.1"),
             key: self.key.clone(),
-        }
-    }
-
-    /// Runs on a thread to accept messages from a ZeroMQ socket connected to
-    /// the kernel and funnel them into an MPSC channel.
-    fn message_proxy_thread(socket: Socket, sender: Sender<Message>) {
-        loop {
-            let message = match Message::read_from_socket(&socket) {
-                Ok(m) => m,
-                Err(err) => {
-                    panic!("Could not read message from socket proxy: {}", err);
-                }
-            };
-            sender.send(message).unwrap();
         }
     }
 }
