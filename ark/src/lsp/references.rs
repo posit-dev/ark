ae9c7602fb11f6601a0c05daf84e5ddc20a72c7a
Kevin Ushey <kevinushey@gmail.com>
2022-06-14 15:53:07 -0700

    separate 'free' symbols from named accessors

--- ark/src/lsp/references.rs
+++ ark/src/lsp/references.rs
@@ -11,6 +11,7 @@ use tower_lsp::lsp_types::Location;
 use tower_lsp::lsp_types::Range;
 use tower_lsp::lsp_types::ReferenceParams;
 use tower_lsp::lsp_types::Url;
+use tree_sitter::Node;
 use tree_sitter::Point;
 use walkdir::DirEntry;
 use walkdir::WalkDir;
@@ -24,7 +25,7 @@ use crate::lsp::traits::point::PointExt;
 use crate::lsp::traits::position::PositionExt;
 
 fn _filter_entry(entry: &DirEntry) -> bool {
-
+    
     // TODO: Figure out if we can read this from the front-end;
     // the user has likely defined a set of workspace file filters
     // that could control which files we search for references in.
@@ -33,32 +34,100 @@ fn _filter_entry(entry: &DirEntry) -> bool {
         ".git" | "node_modules" => false,
         _ => true,
     }
+    
+}
 
+enum ReferenceKind {
+    SymbolName,        // a regular R symbol
+    DollarName,        // a dollar name, following '$'
+    SlotName,          // a slot name, following '@'
+}
+
+struct Context {
+    kind: ReferenceKind,
+    symbol: String,
+}
+
+fn add_reference(node: &Node, path: &Path, locations: &mut Vec<Location>) {
+    let location = Location::new(
+        Url::from_file_path(path).expect("valid path"),
+        Range::new(node.start_position().as_position(), node.end_position().as_position())
+    );
+    locations.push(location);
+}
+
+fn found_match(node: &Node, contents: &str, context: &Context) -> bool {
+    
+    if node.kind() != "identifier" {
+        return false;
+    }
+    
+    let symbol = node.utf8_text(contents.as_bytes()).expect("contents");
+    if symbol != context.symbol {
+        return false;
+    }
+    
+    match context.kind {
+        
+        ReferenceKind::DollarName => {
+            
+            if let Some(sibling) = node.prev_sibling() {
+                if sibling.kind() == "$" {
+                    return true;
+                }
+            }
+            
+        },
+        
+        ReferenceKind::SlotName => {
+            
+            if let Some(sibling) = node.prev_sibling() {
+                if sibling.kind() == "@" {
+                    return true;
+                }
+            }
+        },
+        
+        ReferenceKind::SymbolName => {
+            
+            if let Some(sibling) = node.prev_sibling() {
+                if !matches!(sibling.kind(), "$" | "@") {
+                    return true;
+                }
+            } else {
+                return true;
+            }
+        }
+        
+    }
+
+    return false;
+    
 }
 
 impl Backend {
-
-    fn find_identifier_at_point(&self, uri: &Url, point: Point) -> Result<String, ()> {
-
+    
+    fn build_context(&self, uri: &Url, point: Point) -> Result<Context, ()> {
+        
         // Unwrap the URL.
         let path = unwrap!(uri.to_file_path(), {
             log_push!("URL {} not associated with a local file path", uri);
             return Err(());
         });
-
-        // Figure out the identifier we're looking for.
-        let identifier = self.with_document(path.as_path(), |document| {
         
+        // Figure out the identifier we're looking for.
+        let context = self.with_document(path.as_path(), |document| {
+            
             let ast = unwrap!(document.ast.as_ref(), {
                 log_push!("no ast associated with document {}", path.display());
                 return Err(());
             });
-
+            
             let mut node = unwrap!(ast.root_node().descendant_for_point_range(point, point), {
                 log_push!("couldn't find node associated with point {:?}", point);
                 return Err(());
             });
-
+            
             // Check and see if we got an identifier. If we didn't, we might need to use
             // some heuristics to look around. Unfortunately, it seems like if you double-click
             // to select an identifier, and then use Right Click -> Find All References, the
@@ -74,40 +143,64 @@ impl Backend {
                     return Err(());
                 });
             }
-
+            
             // double check that we found an identifier
             if node.kind() != "identifier" {
                 log_push!("couldn't find an identifier associated with point {:?}", point);
                 return Err(());
             }
+            
+            // check this node's previous sibling; if this is the name of a slot
+            // or dollar accessed item, we should mark it
+            let kind = match node.prev_sibling() {
+                
+                None => {
+                    log_push!("node {:?} has no previous sibling", node);
+                    ReferenceKind::SymbolName
+                },
 
+                Some(sibling) => {
+                    log_push!("found sibling {:?} ({})", sibling, sibling.kind());
+                    match sibling.kind() {
+                        "$" => ReferenceKind::DollarName,
+                        "@" => ReferenceKind::SlotName,
+                        _   => ReferenceKind::SymbolName,
+                    }
+                }
+
+            };
+            
             // return identifier text contents
             let contents = document.contents.to_string();
-            let needle = node.utf8_text(contents.as_bytes()).expect("node contents");
-            return Ok(needle.to_string());
-
+            let symbol = node.utf8_text(contents.as_bytes()).expect("node contents");
+            
+            Ok(Context {
+                kind: kind,
+                symbol: symbol.to_string(),
+            })
+            
         });
-
-        return identifier;
-
+        
+        return context;
+        
     }
-
-    fn find_references_in_folder(&self, identifier: &str, path: &Path, locations: &mut Vec<Location>) {
+    
+    fn find_references_in_folder(&self, context: &Context, path: &Path, locations: &mut Vec<Location>) {
         
         let walker = WalkDir::new(path);
         for entry in walker.into_iter().filter_entry(|entry| _filter_entry(entry)) {
-
+            
             let entry = unwrap!(entry, { continue; });
             let path = entry.path();
             let ext = unwrap!(path.extension(), { continue; });
             if ext != "r" && ext != "R" { continue; }
-
+            
             log_push!("found R file {}", path.display());
             let result = self.with_document(path, |document| {
-                self.find_references_in_document(identifier, path, document, locations);
+                self.find_references_in_document(context, path, document, locations);
                 return Ok(());
             });
-
+            
             match result {
                 Ok(result) => result,
                 Err(_error) => {
@@ -115,70 +208,61 @@ impl Backend {
                     continue;
                 }
             }
-
+            
         }
-
+        
     }
-
-    fn find_references_in_document(&self, identifier: &str, path: &Path, document: &Document, locations: &mut Vec<Location>) {
+    
+    fn find_references_in_document(&self, context: &Context, path: &Path, document: &Document, locations: &mut Vec<Location>) {
         
         // recurse and find symbols of the matching name
         let ast = unwrap!(document.ast.as_ref(), {
             log_push!("no ast available");
             return;
         });
-
+        
         let contents = document.contents.to_string();
-
+        
         let mut cursor = ast.walk();
         cursor.recurse(|node| {
-
-            if node.kind() == "identifier" {
-                let text = node.utf8_text(contents.as_bytes()).expect("contents");
-                if text == identifier {
-                    let location = Location::new(
-                        Url::from_file_path(path).expect("valid path"),
-                        Range::new(node.start_position().as_position(), node.end_position().as_position())
-                    );
-                    locations.push(location);
-                }
+            
+            if found_match(&node, &contents, &context) {
+                add_reference(&node, path, locations);
             }
-
+            
             return true;
-
+            
+            
         });
-
+        
     }
-
+    
     pub(crate) fn find_references(&self, params: ReferenceParams) -> Result<Vec<Location>, ()> {
-
+        
         // Create our locations vector.
         let mut locations : Vec<Location> = Vec::new();
-
+        
         // Extract relevant parameters.
         let uri = params.text_document_position.text_document.uri;
         let point = params.text_document_position.position.as_point();
         
         // Figure out the identifier we're looking for.
-        let identifier = match self.find_identifier_at_point(&uri, point) {
-            Ok(identifier) => identifier,
-            Err(_error) => {
-                log_push!("failed to find identifier at point {}", point);
-                return Err(());
-            }
-        };
-
+        let context = unwrap!(self.build_context(&uri, point), {
+            log_push!("failed to find build context at point {}", point);
+            return Err(());
+        });
+        
         // Now, start searching through workspace folders for references to that identifier.
         if let Ok(workspace) = self.workspace.lock() {
             for folder in workspace.folders.iter() {
                 if let Ok(path) = folder.to_file_path() {
                     log_push!("searching references in folder {}", path.display());
-                    self.find_references_in_folder(&identifier, &path, &mut locations);
+                    self.find_references_in_folder(&context, &path, &mut locations);
                 }
             }
         }
-
-
+        
+        
         return Ok(locations);
         
     }
