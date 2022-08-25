1fd33402ebd16587004ee373f21949acd7c127c7
Jonathan McPherson <jonathan@rstudio.com>
2022-08-25 23:40:07 +0000

    implement detection for R_HOME

--- ark/src/main.rs
+++ ark/src/main.rs
@@ -9,6 +9,7 @@
 
 use crate::control::Control;
 use crate::shell::Shell;
+use crate::version::detect_r;
 use amalthea::connection_file::ConnectionFile;
 use amalthea::kernel::Kernel;
 use amalthea::kernel_spec::KernelSpec;
@@ -63,7 +64,16 @@ fn start_kernel(connection_file: ConnectionFile) {
 
 // Installs the kernelspec JSON file into one of Jupyter's search paths.
 fn install_kernel_spec() {
-    let env = serde_json::Map::new();
+    // Create the environment set for the kernel spec
+    let mut env = serde_json::Map::new();
+
+    // Detect the active version of R and set the R_HOME environment variable
+    // accordingly
+    let r_version = detect_r();
+    env.insert(
+        "R_HOME".to_string(),
+        serde_json::Value::String(r_version.r_home.clone()),
+    );
 
     // Create the kernelspec
     match env::current_exe() {
@@ -81,7 +91,7 @@ fn install_kernel_spec() {
             if let Err(err) = spec.install(String::from("ark")) {
                 eprintln!("Failed to install Ark's Jupyter kernelspec. {}", err);
             } else {
-                println!("Successfully installed Ark Jupyter kernelspec.")
+                println!("Successfully installed Ark Jupyter kernelspec for {}", r_version.r_home);
             }
         }
         Err(err) => {
