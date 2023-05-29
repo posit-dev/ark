3772836c6bbed47052fefcc5349fb16628dd74d4
Romain François <romain@posit.co>
2023-05-29 15:34:44 +0200

    handle when `x` expression deparsed to more than one string

--- crates/ark/src/lsp/modules/data_viewer.R
+++ crates/ark/src/lsp/modules/data_viewer.R
@@ -5,7 +5,10 @@
 #
 #
 
-.ps.view_data_frame <- function(x, title = deparse(substitute(x))) {
+.ps.view_data_frame <- function(x, title) {
+    if (missing(title)) {
+        title <- paste(deparse(substitute(x)), collapse = "")
+    }
     stopifnot(
         is.data.frame(x) || is.matrix(x),
         is.character(title) && length(title) == 1L
