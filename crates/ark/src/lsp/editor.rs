1f9c3b7ed0cc4e31332852a298c3dd0e69e221f8
Davis Vaughan <davis@posit.co>
2023-07-14 15:25:01 -0400

    Make Rust side of editor handler more robust

--- crates/ark/src/lsp/editor.rs
+++ crates/ark/src/lsp/editor.rs
@@ -8,6 +8,7 @@
 use harp::vector::CharacterVector;
 use harp::vector::Vector;
 use libR_sys::*;
+use stdext::unwrap;
 use tokio::runtime::Runtime;
 use tower_lsp::lsp_types::ShowDocumentParams;
 use tower_lsp::lsp_types::Url;
@@ -19,13 +20,25 @@ unsafe extern "C" fn ps_editor(file: SEXP, _title: SEXP) -> SEXP {
     let rt = Runtime::new().unwrap();
     let globals = R_CALLBACK_GLOBALS.as_ref().unwrap();
     let files = CharacterVector::new_unchecked(file);
+
     for file in files.iter() {
         if let Some(file) = file {
             rt.block_on(async move {
+                let uri = Url::from_file_path(&file);
+
+                let uri = unwrap!(uri, Err(_) => {
+                    // The R side of this handles most issues, but we don't want to panic
+                    // if some unknown file path slips through.
+                    // `from_file_path()` doesn't return `Display`able errors, so we
+                    // can't necessarily give a good reason.
+                    log::error!("Can't open file at '{}'.", file);
+                    return;
+                });
+
                 globals
                     .lsp_client
                     .show_document(ShowDocumentParams {
-                        uri: Url::from_file_path(file).unwrap(),
+                        uri,
                         external: Some(false),
                         take_focus: Some(true),
                         selection: None,
@@ -36,5 +49,5 @@ unsafe extern "C" fn ps_editor(file: SEXP, _title: SEXP) -> SEXP {
         }
     }
 
-    file
+    R_NilValue
 }
