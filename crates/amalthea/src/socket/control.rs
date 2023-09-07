0d5e5fa71044d62729055a242ff7d4ddb09e2b28
Davis Vaughan <davis@posit.co>
2023-09-07 11:15:40 -0400

    Emit busy/idle status from Control requests

--- crates/amalthea/src/socket/control.rs
+++ crates/amalthea/src/socket/control.rs
@@ -8,6 +8,7 @@
 use std::sync::Arc;
 use std::sync::Mutex;
 
+use crossbeam::channel::SendError;
 use crossbeam::channel::Sender;
 use futures::executor::block_on;
 use log::error;
@@ -17,11 +18,20 @@ use log::warn;
 
 use crate::error::Error;
 use crate::language::control_handler::ControlHandler;
+use crate::socket::iopub::IOPubContextChannel;
+use crate::socket::iopub::IOPubMessage;
 use crate::socket::socket::Socket;
+use crate::traits::iopub::IOPubSenderExt;
+use crate::wire::interrupt_request::InterruptRequest;
+use crate::wire::jupyter_message::JupyterMessage;
 use crate::wire::jupyter_message::Message;
+use crate::wire::jupyter_message::ProtocolMessage;
+use crate::wire::shutdown_request::ShutdownRequest;
+use crate::wire::status::ExecutionState;
 
 pub struct Control {
     socket: Socket,
+    iopub_tx: Sender<IOPubMessage>,
     handler: Arc<Mutex<dyn ControlHandler>>,
     stdin_interrupt_tx: Sender<bool>,
 }
@@ -29,11 +39,13 @@ pub struct Control {
 impl Control {
     pub fn new(
         socket: Socket,
+        iopub_tx: Sender<IOPubMessage>,
         handler: Arc<Mutex<dyn ControlHandler>>,
         stdin_interrupt_tx: Sender<bool>,
     ) -> Self {
         Self {
             socket,
+            iopub_tx,
             handler,
             stdin_interrupt_tx,
         }
@@ -52,43 +64,91 @@ impl Control {
                 },
             };
 
-            match message {
-                Message::ShutdownRequest(req) => {
-                    info!("Received shutdown request, shutting down kernel: {:?}", req);
-
-                    // Lock the shell handler object on this thread
-                    let shell_handler = self.handler.lock().unwrap();
-                    if let Err(err) = block_on(shell_handler.handle_shutdown_request(&req.content))
-                    {
-                        warn!("Failed to handle shutdown request: {:?}", err);
-                        // TODO: if this fails, maybe we need to force a process shutdown?
-                    }
-                    break;
-                },
-                Message::InterruptRequest(req) => {
-                    info!(
-                        "Received interrupt request, asking kernel to stop: {:?}",
-                        req
-                    );
-
-                    // Notify StdIn socket first in case it's waiting for
-                    // input which is never going to come because of the
-                    // interrupt
-                    if let Err(err) = self.stdin_interrupt_tx.send(true) {
-                        error!("Failed to send interrupt request: {:?}", err);
-                    }
-
-                    let control_handler = self.handler.lock().unwrap();
-                    if let Err(err) = block_on(control_handler.handle_interrupt_request()) {
-                        error!("Failed to handle interrupt request: {:?}", err);
-                    }
-                    // TODO: What happens if the interrupt isn't handled?
-                },
-                _ => warn!(
-                    "{}",
-                    Error::UnsupportedMessage(message, String::from("Control"))
-                ),
+            if let Err(err) = self.process_message(message) {
+                warn!("Could not handle control message: {err}");
             }
         }
+    }
+
+    fn process_message(&self, message: Message) -> Result<(), Error> {
+        match message {
+            Message::ShutdownRequest(req) => {
+                self.handle_request(req, |r| self.handle_shutdown_request(r))
+            },
+            Message::InterruptRequest(req) => {
+                self.handle_request(req, |r| self.handle_interrupt_request(r))
+            },
+            _ => Err(Error::UnsupportedMessage(message, String::from("control"))),
+        }
+    }
+
+    fn send_state<T: ProtocolMessage>(
+        &self,
+        parent: JupyterMessage<T>,
+        state: ExecutionState,
+    ) -> Result<(), SendError<IOPubMessage>> {
+        self.iopub_tx
+            .send_state(parent, IOPubContextChannel::Control, state)
+    }
+
+    fn handle_request<T, H>(&self, req: JupyterMessage<T>, handler: H) -> Result<(), Error>
+    where
+        T: ProtocolMessage,
+        H: FnOnce(JupyterMessage<T>) -> Result<(), Error>,
+    {
+        // Enter the kernel-busy state in preparation for handling the message.
+        if let Err(err) = self.send_state(req.clone(), ExecutionState::Busy) {
+            warn!("Failed to change kernel status to busy: {err}");
+        }
+
+        // Call amalthea side of the handler.
+        let result = handler(req.clone());
+
+        // Return to idle -- we always do this, even if the message generated an
+        // error, since many front ends won't submit additional messages until
+        // the kernel is marked idle.
+        if let Err(err) = self.send_state(req, ExecutionState::Idle) {
+            warn!("Failed to restore kernel status to idle: {err}");
+        }
+
+        return result;
+    }
+
+    fn handle_shutdown_request(&self, req: JupyterMessage<ShutdownRequest>) -> Result<(), Error> {
+        info!("Received shutdown request, shutting down kernel: {:?}", req);
+
+        // Lock the control handler object on this thread
+        let control_handler = self.handler.lock().unwrap();
+
+        if let Err(err) = block_on(control_handler.handle_shutdown_request(&req.content)) {
+            warn!("Failed to handle shutdown request: {:?}", err);
+            // TODO: if this fails, maybe we need to force a process shutdown?
+        }
+
+        Ok(())
+    }
+
+    fn handle_interrupt_request(&self, req: JupyterMessage<InterruptRequest>) -> Result<(), Error> {
+        info!(
+            "Received interrupt request, asking kernel to stop: {:?}",
+            req
+        );
+
+        // Notify StdIn socket first in case it's waiting for
+        // input which is never going to come because of the
+        // interrupt
+        if let Err(err) = self.stdin_interrupt_tx.send(true) {
+            error!("Failed to send interrupt request: {:?}", err);
+        }
+
+        // Lock the control handler object on this thread
+        let control_handler = self.handler.lock().unwrap();
+
+        if let Err(err) = block_on(control_handler.handle_interrupt_request()) {
+            error!("Failed to handle interrupt request: {:?}", err);
+            // TODO: What happens if the interrupt isn't handled?
+        }
+
+        Ok(())
     }
 }
