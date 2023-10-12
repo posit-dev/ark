ec3d6070e2e10dab5dcae36c267e525093e94e48
Lionel Henry <lionel.hry@gmail.com>
2023-10-12 18:12:41 +0200

    Reply to shutdown requests

--- crates/amalthea/src/socket/control.rs
+++ crates/amalthea/src/socket/control.rs
@@ -125,10 +125,25 @@ impl Control {
         // Lock the control handler object on this thread
         let control_handler = self.handler.lock().unwrap();
 
-        if let Err(err) = block_on(control_handler.handle_shutdown_request(&req.content)) {
-            warn!("Failed to handle shutdown request: {:?}", err);
-            // TODO: if this fails, maybe we need to force a process shutdown?
-        }
+        let reply = unwrap!(
+            block_on(control_handler.handle_shutdown_request(&req.content)),
+            Err(err) => {
+                log::error!("Failed to handle shutdown request: {err:?}");
+                return Ok(())
+                // TODO: if this fails, maybe we need to force a process shutdown?
+            }
+        );
+
+        // TODO: This currently races with the R thread exiting the
+        // REPL and calling `exit()`. Odds are that this is never sent.
+        // We should send this from the `R_CleanUp()` frontend method
+        // once we implement it.
+        unwrap!(
+            req.send_reply(reply, &self.socket),
+            Err(err) => {
+                log::error!("Failed to reply to interrupt request: {err:?}");
+            }
+        );
 
         Ok(())
     }
