a08f78e2b212438a5298e6d370ad542c0a7ebc1b
Jackie Gutman <jacqueline.gutman@rstudio.com>
2023-10-19 10:12:08 -0400

    register our hook for view

--- crates/ark/src/modules/public/r_data_viewer.R
+++ crates/ark/src/modules/public/r_data_viewer.R
@@ -7,7 +7,7 @@
 
 .ps.view_data_frame <- function(x, title) {
     if (missing(title)) {
-        title <- paste(deparse(substitute(x)), collapse = "")
+        title <- paste(deparse(substitute(x), backtick=TRUE), collapse = "")
     }
     stopifnot(
         is.data.frame(x) || is.matrix(x),
@@ -15,3 +15,18 @@
     )
     invisible(.ps.Call("ps_view_data_frame", x, title))
 }
+
+.ps.register_utils_hook <- function(name, hook) {
+    packageName <- "package:utils"
+    original <- base::get(name, packageName, mode="function")
+    # check if function exists
+    if (is.null(original)) {
+        fmt <- "internal error: function utils::%s not found"
+        msg <- sprintf(fmt, shQuote(name))
+        stop(msg, call. = FALSE)
+    }
+    utilsEnv <- as.environment(packageName)
+    .ps.binding.replace(name, hook, utilsEnv)
+}
+
+.ps.register_utils_hook("View", .ps.view_data_frame)
