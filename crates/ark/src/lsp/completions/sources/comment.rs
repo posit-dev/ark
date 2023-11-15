e8f382149d4b824f847a60ab202e1414bb6c9eae
Davis Vaughan <davis@posit.co>
2023-11-15 16:49:13 -0500

    Add tests to `comment.rs`

--- crates/ark/src/lsp/completions/sources/comment.rs
+++ crates/ark/src/lsp/completions/sources/comment.rs
@@ -11,7 +11,6 @@ use anyhow::Result;
 use harp::exec::RFunction;
 use harp::exec::RFunctionExt;
 use regex::Regex;
-use stdext::unwrap;
 use tower_lsp::lsp_types::CompletionItem;
 use tower_lsp::lsp_types::Documentation;
 use tower_lsp::lsp_types::InsertTextFormat;
@@ -70,35 +69,134 @@ pub fn completions_from_comment(context: &DocumentContext) -> Result<Option<Vec<
 
     let items = doc.as_vec().unwrap();
     for entry in items.iter() {
-        let name = unwrap!(entry["name"].as_str(), None => {
+        let Some(name) = entry["name"].as_str() else {
             continue;
-        });
+        };
 
-        let label = name.to_string();
-        let mut item = completion_item(label.clone(), CompletionData::RoxygenTag {
-            tag: label.clone(),
-        })?;
-
-        // TODO: What is the appropriate icon for us to use here?
         let template = entry["template"].as_str();
-        if let Some(template) = template {
-            item.insert_text_format = Some(InsertTextFormat::SNIPPET);
-            item.insert_text = Some(format!("{}{}", name, template));
-        } else {
-            item.insert_text = Some(format!("@{}", label.as_str()));
-        }
+        let description = entry["description"].as_str();
 
-        item.detail = Some(format!("roxygen @{} (R)", name));
-        if let Some(description) = entry["description"].as_str() {
-            let markup = MarkupContent {
-                kind: MarkupKind::Markdown,
-                value: description.to_string(),
-            };
-            item.documentation = Some(Documentation::MarkupContent(markup));
-        }
+        let item = completion_item_from_roxygen(name, template, description)?;
 
         completions.push(item);
     }
 
     Ok(Some(completions))
+}
+
+fn completion_item_from_roxygen(
+    name: &str,
+    template: Option<&str>,
+    description: Option<&str>,
+) -> Result<CompletionItem> {
+    let label = name.to_string();
+
+    let mut item = completion_item(label.clone(), CompletionData::RoxygenTag {
+        tag: label.clone(),
+    })?;
+
+    // TODO: What is the appropriate icon for us to use here?
+    if let Some(template) = template {
+        item.insert_text_format = Some(InsertTextFormat::SNIPPET);
+        item.insert_text = Some(format!("{name}{template}"));
+    } else {
+        item.insert_text = Some(format!("{label}"));
+    }
+
+    item.detail = Some(format!("roxygen @{} (R)", name));
+    if let Some(description) = description {
+        let markup = MarkupContent {
+            kind: MarkupKind::Markdown,
+            value: description.to_string(),
+        };
+        item.documentation = Some(Documentation::MarkupContent(markup));
+    }
+
+    Ok(item)
+}
+
+#[test]
+fn test_comment() {
+    use tree_sitter::Point;
+
+    use crate::lsp::documents::Document;
+    use crate::test::r_test;
+
+    r_test(|| {
+        // If not in a comment, return `None`
+        let point = Point { row: 0, column: 1 };
+        let document = Document::new("mean()");
+        let context = DocumentContext::new(&document, point);
+        let completions = completions_from_comment(&context).unwrap();
+        assert!(completions.is_none());
+
+        // If in a comment, return empty vector
+        let point = Point { row: 0, column: 1 };
+        let document = Document::new("# mean");
+        let context = DocumentContext::new(&document, point);
+        let completions = completions_from_comment(&context).unwrap().unwrap();
+        assert!(completions.is_empty());
+    });
+}
+
+#[test]
+fn test_roxygen_comment() {
+    use libR_sys::LOGICAL_ELT;
+    use tree_sitter::Point;
+
+    use crate::lsp::documents::Document;
+    use crate::test::r_test;
+
+    r_test(|| unsafe {
+        let installed = RFunction::new("", ".ps.is_installed")
+            .add("roxygen2")
+            .add("7.2.1.9000")
+            .call()
+            .unwrap();
+        let installed = LOGICAL_ELT(*installed, 0) != 0;
+
+        if !installed {
+            return;
+        }
+
+        let point = Point { row: 0, column: 4 };
+        let document = Document::new("#' @");
+        let context = DocumentContext::new(&document, point);
+        let completions = completions_from_comment(&context).unwrap().unwrap();
+
+        let completions: Vec<CompletionItem> = completions
+            .into_iter()
+            .filter(|item| item.label == "aliases")
+            .collect();
+
+        // roxygen2 controls the contents of the fields, so we won't test those.
+        // Just make sure we found it!
+        assert_eq!(completions.len(), 1);
+    });
+}
+
+#[test]
+fn test_roxygen_completion_item() {
+    let name = "aliases";
+    let template = " ${1:alias}";
+    let description = "Add additional aliases to the topic.";
+
+    // With all optional details
+    let item = completion_item_from_roxygen(name, Some(template), Some(description)).unwrap();
+    assert_eq!(item.label, name);
+    assert_eq!(item.detail, Some("roxygen @aliases (R)".to_string()));
+    assert_eq!(item.insert_text, Some("aliases ${1:alias}".to_string()));
+
+    let markup = Documentation::MarkupContent(MarkupContent {
+        kind: MarkupKind::Markdown,
+        value: description.to_string(),
+    });
+    assert_eq!(item.documentation, Some(markup));
+
+    // Without optional details
+    let name = "export";
+    let item = completion_item_from_roxygen(name, None, None).unwrap();
+    assert_eq!(item.label, name);
+    assert_eq!(item.insert_text, Some("export".to_string()));
+    assert_eq!(item.documentation, None);
 }
