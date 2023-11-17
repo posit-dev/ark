bb4c10970d2f6e235e749987af3862c5a0d4d6e7
Jonathan McPherson <jonathan@rstudio.com>
2023-11-17 09:40:16 -0800

    use r_unwrap across entire rust stack

--- crates/ark/src/html_widget.rs
+++ crates/ark/src/html_widget.rs
@@ -9,49 +9,44 @@ use std::result::Result::Ok;
 
 use amalthea::socket::iopub::IOPubMessage;
 use amalthea::wire::display_data::DisplayData;
-use anyhow::*;
 use harp::exec::r_unwrap;
 use harp::object::RObject;
 use libR_sys::R_NilValue;
 use libR_sys::SEXP;
-use log::warn;
 use serde_json::Value;
 
 use crate::interface::RMain;
 #[harp::register]
 pub unsafe extern "C" fn ps_html_widget(kind: SEXP, tags: SEXP) -> SEXP {
-    // For friendly display: the class/kind of the widget
-    let widget_class = match String::try_from(RObject::view(kind)) {
-        Ok(kind) => kind,
-        Err(err) => {
-            warn!("Failed to convert HTML widget class to string: {:?}", err);
-            String::new()
-        },
-    };
+    // Convert Rust errors during serialization to R errors
+    r_unwrap(|| -> Result<(), anyhow::Error> {
+        // For friendly display: the class/kind of the widget
+        let widget_class = String::try_from(RObject::view(kind))?;
 
-    // Convert the tags to JSON for display
-    let json = r_unwrap(|| match Value::try_from(RObject::view(tags)) {
-        Ok(val) => Ok(val),
-        Err(err) => Err(anyhow!(err).context("Failed to convert HTML widget tags to JSON")),
+        // Convert the tags to JSON for display
+        let json = Value::try_from(RObject::view(tags))?;
+
+        // Get the IOPub channel
+        let main = RMain::get();
+        let iopub_tx = main.get_iopub_tx().clone();
+
+        // Create the output object
+        let output = serde_json::json!({
+            "application/vnd.r.htmlwidget": json,
+            "text/plain": format!("<{} HTML widget>", widget_class)
+        });
+
+        // Emit the HTML output on IOPub for delivery to the client
+        let message = IOPubMessage::DisplayData(DisplayData {
+            data: output,
+            metadata: serde_json::Value::Null,
+            transient: serde_json::Value::Null,
+        });
+
+        iopub_tx.send(message)?;
+
+        Ok(())
     });
-
-    // Get the IOPub channel
-    let main = RMain::get();
-    let iopub_tx = main.get_iopub_tx().clone();
-
-    // Create the output object
-    let output = serde_json::json!({
-        "application/vnd.r.htmlwidget": json,
-        "text/plain": format!("<{} HTML widget>", widget_class)
-    });
-
-    // Emit the HTML output on IOPub for delivery to the client
-    let message = IOPubMessage::DisplayData(DisplayData {
-        data: output,
-        metadata: serde_json::Value::Null,
-        transient: serde_json::Value::Null,
-    });
-    iopub_tx.send(message).unwrap();
 
     R_NilValue
 }
--- crates/ark/src/json.rs
+++ crates/ark/src/json.rs
@@ -1,29 +1,29 @@
 //
-// viewer.rs
+// json.rs
 //
 // Copyright (C) 2023 Posit Software, PBC. All rights reserved.
 //
 //
 
-use std::ffi::CString;
-
+use harp::exec::r_unwrap;
 use harp::object::RObject;
-use libR_sys::Rf_error;
 use libR_sys::SEXP;
 
+/// Convenience method to convert a JSON object to a string
 #[harp::register]
 pub unsafe extern "C" fn ps_to_json(obj: SEXP) -> SEXP {
-    let obj = RObject::view(obj);
-    match serde_json::Value::try_from(obj) {
-        Ok(value) => {
-            // Serialize the value to a string
-            let json = serde_json::to_string_pretty(&value).unwrap();
-            RObject::try_from(json).unwrap().sexp
-        },
-        Err(err) => {
-            let err = format!("Failed to convert: {:?}", err);
-            let msg = CString::new(err).unwrap();
-            Rf_error(msg.as_ptr())
-        },
-    }
+    r_unwrap(|| -> Result<SEXP, anyhow::Error> {
+        // Create a view of the object to be serialized
+        let obj = RObject::view(obj);
+
+        // Convert the object to a JSON value; this is the core serialization step
+        let val = serde_json::Value::try_from(obj)?;
+
+        // Format the JSON value as a string for display
+        let json = serde_json::to_string_pretty(&val)?;
+
+        // Create an R string from the JSON string
+        let robj = RObject::try_from(json)?;
+        Ok(robj.sexp)
+    })
 }
