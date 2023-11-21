92ab6bab8108817b06b88120f201837cb466718a
Jonathan McPherson <jonathan@rstudio.com>
2023-11-20 17:17:45 -0800

    add frontend comm types for rpcs

--- crates/amalthea/src/comm/frontend_comm.rs
+++ crates/amalthea/src/comm/frontend_comm.rs
@@ -7,6 +7,7 @@
 
 use serde::Deserialize;
 use serde::Serialize;
+use serde_json::Value;
 
 use crate::wire::client_event::ClientEvent;
 
@@ -14,4 +15,33 @@ use crate::wire::client_event::ClientEvent;
 #[serde(tag = "msg_type", rename_all = "snake_case")]
 pub enum FrontendMessage {
     Event(ClientEvent),
+    RpcRequest(FrontendRpcRequest),
+    RpcResultResponse(FrontendRpcResult),
+    RpcResultError(FrontendRpcError),
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(tag = "msg_type", rename_all = "snake_case")]
+pub enum FrontendRpcRequest {
+    Method(String),
+    Params(Vec<Value>),
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(tag = "msg_type", rename_all = "snake_case")]
+pub enum FrontendRpcResult {
+    Result(Value),
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(tag = "msg_type", rename_all = "snake_case")]
+pub enum FrontendRpcErrorData {
+    Message(String),
+    Code(i32),
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(tag = "msg_type", rename_all = "snake_case")]
+pub enum FrontendRpcError {
+    Error(FrontendRpcErrorData),
 }
