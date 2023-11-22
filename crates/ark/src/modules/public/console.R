66e66c709a866a43ef3e7822919e661ca80fd474
Jonathan McPherson <jonathan@rstudio.com>
2023-11-22 15:32:26 -0800

    add RPC tests to test suite

---
+++ crates/ark/src/modules/public/console.R
@@ -0,0 +1,10 @@
+#
+# console.R
+#
+# Copyright (C) 2023 Posit Software, PBC. All rights reserved.
+#
+#
+
+.ps.rpc.setConsoleWidth <- function(width) {
+    options(width = width)
+}
---
+++ crates/ark/tests/frontend.rs
@@ -0,0 +1,109 @@
+//
+// frontend.rs
+//
+// Copyright (C) 2023 Posit Software, PBC. All rights reserved.
+//
+//
+
+use amalthea::comm::comm_channel::CommChannelMsg;
+use amalthea::comm::frontend_comm::FrontendMessage;
+use amalthea::comm::frontend_comm::FrontendRpcRequest;
+use amalthea::comm::frontend_comm::FrontendRpcResult;
+use amalthea::events::BusyEvent;
+use amalthea::events::PositronEvent;
+use amalthea::socket::comm::CommInitiator;
+use amalthea::socket::comm::CommSocket;
+use ark::frontend::frontend::PositronFrontend;
+use ark::r_task;
+use ark::test::r_test;
+use harp::exec::RFunction;
+use harp::exec::RFunctionExt;
+use harp::object::RObject;
+use serde_json::Value;
+
+/**
+ * Basic test for the frontend comm.
+ */
+#[test]
+fn test_frontend_comm() {
+    r_test(|| {
+        // Create a sender/receiver pair for the comm channel.
+        let comm = CommSocket::new(
+            CommInitiator::FrontEnd,
+            String::from("test-frontend-comm-id"),
+            String::from("positron.frontend"),
+        );
+
+        // Create a frontend instance
+        let frontend = PositronFrontend::start(comm.clone());
+
+        // Get the current console width
+        let old_width = r_task(|| unsafe {
+            let width = RFunction::from("getOption")
+                .param("x", "width")
+                .call()
+                .unwrap();
+            RObject::to::<i32>(width).unwrap()
+        });
+
+        // Send a message to the frontend
+        let id = String::from("test-id-1");
+        let request = FrontendMessage::RpcRequest(FrontendRpcRequest {
+            method: String::from("setConsoleWidth"),
+            params: vec![Value::from(123)],
+        });
+        comm.incoming_tx
+            .send(CommChannelMsg::Rpc(
+                id,
+                serde_json::to_value(request).unwrap(),
+            ))
+            .unwrap();
+
+        // Wait for the reply; this should be a FrontendRpcResult. We don't wait
+        // more than a second since this should be quite fast and we don't want to
+        // hang the test suite if it doesn't return.
+        let response = comm
+            .outgoing_rx
+            .recv_timeout(std::time::Duration::from_secs(1))
+            .unwrap();
+        match response {
+            CommChannelMsg::Rpc(id, result) => {
+                println!("Got RPC result: {:?}", result);
+                serde_json::from_value::<FrontendRpcResult>(result).unwrap();
+                assert_eq!(id, "test-id-1")
+            },
+            _ => panic!("Unexpected response: {:?}", response),
+        }
+
+        // Get the new console width
+        let new_width = r_task(|| unsafe {
+            let width = RFunction::from("getOption")
+                .param("x", "width")
+                .call()
+                .unwrap();
+            RObject::to::<i32>(width).unwrap()
+        });
+
+        // Assert that the console width changed
+        assert_eq!(new_width, 123);
+
+        // Send another message to restore the console width as a test cleanup
+        let id = String::from("test-id-2");
+        let request = FrontendMessage::RpcRequest(FrontendRpcRequest {
+            method: String::from("setConsoleWidth"),
+            params: vec![Value::from(old_width)],
+        });
+        comm.incoming_tx
+            .send(CommChannelMsg::Rpc(
+                id,
+                serde_json::to_value(request).unwrap(),
+            ))
+            .unwrap();
+
+        // Mark not busy (this prevents the frontend comm from being closed due to
+        // the Sender being dropped)
+        frontend
+            .send(PositronEvent::Busy(BusyEvent { busy: false }))
+            .unwrap();
+    });
+}
