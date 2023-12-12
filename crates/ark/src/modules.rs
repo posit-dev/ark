97708ae16056e088441ded120f30752f2430da7d
Davis Vaughan <davis@rstudio.com>
2023-12-12 09:19:10 -0500

    Build Ark for Windows in a GitHub Actions Workflow (#175)
    * Throw some stuff at the wall
    
    * Temporarily add pull-request to fire once
    
    * Now revert since `gh workflow list` finds it
    
    * Tweak
    
    * Tweaks
    
    * Tweak
    
    * Tweak
    
    * Special windows release for iterating quickly
    
    * Remove pull-request again
    
    * Supposedly no `R-4.3.2/` folder
    
    * Use a hopefully more reliable `dll2lib.R`
    
    * No amalthea testing right now
    
    * Also `nologo` the `lib.exe` call
    
    * Windows builds can probably be parallel
    
    Since they aren't on the same worker
    
    * Move back to `release.yml` and make archive
    
    * Try back on `windows-release.yml`
    
    * Slightly more readable
    
    * Upload Windows kernel as a release artifact
    
    * Use platform independent path creation
    
    * Add ark modules to zip
    
    * Oh yea, this is a Mac
    
    * Clean up
    
    * Bump ark

--- crates/ark/src/modules.rs
+++ crates/ark/src/modules.rs
@@ -166,8 +166,8 @@ pub unsafe fn initialize(testing: bool) -> anyhow::Result<()> {
     // look in the source tree (found via the 'CARGO_MANIFEST_DIR' environment
     // variable).
     if !root.exists() {
-        let source = format!("{}/src/modules", env!("CARGO_MANIFEST_DIR"));
-        root = Path::new(&source).to_path_buf();
+        let source = env!("CARGO_MANIFEST_DIR");
+        root = Path::new(&source).join("src").join("modules").to_path_buf();
     }
 
     // Import all module files.
