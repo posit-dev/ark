71ca41faa561b3f268a799153eeeda44cd1866c5
Lionel Henry <lionel.hry@gmail.com>
2023-12-19 16:00:53 +0100

    Switch to Frontend message spec

--- crates/ark/src/frontend/frontend.rs
+++ crates/ark/src/frontend/frontend.rs
@@ -5,40 +5,33 @@
 //
 //
 
-use amalthea::comm::base_comm::JsonRpcErrorCode;
 use amalthea::comm::comm_channel::CommMsg;
-use amalthea::comm::frontend_comm::FrontendMessage;
-use amalthea::comm::frontend_comm::FrontendRpcError;
-use amalthea::comm::frontend_comm::FrontendRpcErrorData;
-use amalthea::comm::frontend_comm::FrontendRpcRequest;
-use amalthea::comm::frontend_comm::FrontendRpcResult;
-use amalthea::events::PositronEvent;
+use amalthea::comm::frontend_comm::FrontendEvent;
 use amalthea::socket::comm::CommSocket;
-use amalthea::wire::client_event::ClientEvent;
 use crossbeam::channel::Receiver;
 use crossbeam::channel::Sender;
 use crossbeam::select;
-use harp::exec::RFunction;
-use harp::exec::RFunctionExt;
-use harp::object::RObject;
+// use harp::exec::RFunction;
+// use harp::exec::RFunctionExt;
+// use harp::object::RObject;
 use log::info;
-use serde_json::Value;
+// use serde_json::Value;
 use stdext::spawn;
 
-use crate::r_task;
+// use crate::r_task;
 
 /// PositronFrontend is a wrapper around a comm channel whose lifetime matches
 /// that of the Positron front end. It is used to perform communication with the
 /// front end that isn't scoped to any particular view.
 pub struct PositronFrontend {
     comm: CommSocket,
-    event_rx: Receiver<PositronEvent>,
+    event_rx: Receiver<FrontendEvent>,
 }
 
 impl PositronFrontend {
-    pub fn start(comm: CommSocket) -> Sender<PositronEvent> {
+    pub fn start(comm: CommSocket) -> Sender<FrontendEvent> {
         // Create a sender-receiver pair for Positron global events
-        let (event_tx, event_rx) = crossbeam::channel::unbounded::<PositronEvent>();
+        let (event_tx, event_rx) = crossbeam::channel::unbounded::<FrontendEvent>();
 
         spawn!("ark-comm-frontend", move || {
             let frontend = Self {
@@ -87,16 +80,11 @@ impl PositronFrontend {
         }
     }
 
-    fn dispatch_event(&self, event: &PositronEvent) {
-        // Convert the event to a client event that the frontend can understand
-        let comm_evt = ClientEvent::try_from(event.clone()).unwrap();
-
-        // Convert the client event to a message we can send to the front end
-        let frontend_evt = FrontendMessage::Event(comm_evt);
-        let comm_msg = CommMsg::Data(serde_json::to_value(frontend_evt).unwrap());
+    fn dispatch_event(&self, event: &FrontendEvent) {
+        let json = serde_json::to_value(event).unwrap();
 
         // Deliver the event to the front end over the comm channel
-        if let Err(err) = self.comm.outgoing_tx.send(comm_msg) {
+        if let Err(err) = self.comm.outgoing_tx.send(CommMsg::Data(json)) {
             log::error!("Error sending Positron event to front end: {}", err);
         };
     }
@@ -119,101 +107,102 @@ impl PositronFrontend {
                 // thread exit.
                 false
             },
-            CommMsg::Rpc(id, request) => {
-                let message = match serde_json::from_value::<FrontendMessage>(request.clone()) {
-                    Ok(msg) => msg,
-                    Err(err) => {
-                        log::warn!("Error decoding RPC request from front end: {:?}", err);
-                        return true;
-                    },
-                };
-                match message {
-                    FrontendMessage::RpcRequest(req) => {
-                        if let Err(err) = self.handle_rpc_request(id, &req) {
-                            log::warn!("Error handling RPC request from front end: {:?}", err);
-                        }
-                    },
-                    _ => {
-                        log::warn!("Unexpected RPC message from front end: {:?}", message);
-                    },
-                };
-                true
+            CommMsg::Rpc(_id, _request) => {
+                todo!();
+                // let message = match serde_json::from_value::<FrontendMessage>(request.clone()) {
+                //     Ok(msg) => msg,
+                //     Err(err) => {
+                //         log::warn!("Error decoding RPC request from front end: {:?}", err);
+                //         return true;
+                //     },
+                // };
+                // match message {
+                //     FrontendMessage::RpcRequest(req) => {
+                //         if let Err(err) = self.handle_rpc_request(id, &req) {
+                //             log::warn!("Error handling RPC request from front end: {:?}", err);
+                //         }
+                //     },
+                //     _ => {
+                //         log::warn!("Unexpected RPC message from front end: {:?}", message);
+                //     },
+                // };
+                // true
             },
         }
     }
 
-    /**
-     * Handles an RPC request from the front end.
-     */
-    fn handle_rpc_request(
-        &self,
-        id: &str,
-        request: &FrontendRpcRequest,
-    ) -> Result<(), anyhow::Error> {
-        // Today, all RPCs are fulfilled by R directly. Check to see if an R
-        // method of the appropriate name is defined.
-        //
-        // Consider: In the future, we may want to allow requests to be
-        // fulfilled here on the Rust side, with only some requests forwarded to
-        // R; Rust methods may wish to establish their own RPC handlers.
+    // /**
+    //  * Handles an RPC request from the front end.
+    //  */
+    // fn handle_rpc_request(
+    //     &self,
+    //     id: &str,
+    //     request: &FrontendRpcRequest,
+    // ) -> Result<(), anyhow::Error> {
+    //     // Today, all RPCs are fulfilled by R directly. Check to see if an R
+    //     // method of the appropriate name is defined.
+    //     //
+    //     // Consider: In the future, we may want to allow requests to be
+    //     // fulfilled here on the Rust side, with only some requests forwarded to
+    //     // R; Rust methods may wish to establish their own RPC handlers.
 
-        // The method name is prefixed with ".ps.rpc.", by convention
-        let method = format!(".ps.rpc.{}", request.method);
+    //     // The method name is prefixed with ".ps.rpc.", by convention
+    //     let method = format!(".ps.rpc.{}", request.method);
 
-        // Use the `exists` function to see if the method exists
-        let exists = r_task(|| unsafe {
-            let exists = RFunction::from("exists")
-                .param("x", method.clone())
-                .call()?;
-            RObject::to::<bool>(exists)
-        })?;
+    //     // Use the `exists` function to see if the method exists
+    //     let exists = r_task(|| unsafe {
+    //         let exists = RFunction::from("exists")
+    //             .param("x", method.clone())
+    //             .call()?;
+    //         RObject::to::<bool>(exists)
+    //     })?;
 
-        if !exists {
-            // No such method; return an error
-            let reply = FrontendMessage::RpcResultError(FrontendRpcError {
-                id: id.to_string(),
-                error: FrontendRpcErrorData {
-                    code: JsonRpcErrorCode::MethodNotFound, // Method not found
-                    message: format!("No such method: {}", request.method),
-                },
-            });
-            let comm_msg = CommMsg::Rpc(id.to_string(), serde_json::to_value(reply)?);
-            self.comm.outgoing_tx.send(comm_msg)?;
-            return Ok(());
-        }
+    //     if !exists {
+    //         // No such method; return an error
+    //         let reply = FrontendMessage::RpcResultError(FrontendRpcError {
+    //             id: id.to_string(),
+    //             error: FrontendRpcErrorData {
+    //                 code: JsonRpcErrorCode::MethodNotFound, // Method not found
+    //                 message: format!("No such method: {}", request.method),
+    //             },
+    //         });
+    //         let comm_msg = CommMsg::Rpc(id.to_string(), serde_json::to_value(reply)?);
+    //         self.comm.outgoing_tx.send(comm_msg)?;
+    //         return Ok(());
+    //     }
 
-        // Form an R function call from the request
-        let result = r_task(|| {
-            let mut call = RFunction::from(method);
-            for param in request.params.iter() {
-                let p = RObject::try_from(param.clone())?;
-                call.add(p);
-            }
-            let result = call.call()?;
-            Value::try_from(result)
-        });
+    //     // Form an R function call from the request
+    //     let result = r_task(|| {
+    //         let mut call = RFunction::from(method);
+    //         for param in request.params.iter() {
+    //             let p = RObject::try_from(param.clone())?;
+    //             call.add(p);
+    //         }
+    //         let result = call.call()?;
+    //         Value::try_from(result)
+    //     });
 
-        // Convert the reply to a message we can send to the front end
-        let reply = match result {
-            Ok(value) => FrontendMessage::RpcResultResponse(FrontendRpcResult {
-                id: id.to_string(),
-                result: value,
-            }),
-            Err(err) => FrontendMessage::RpcResultError(FrontendRpcError {
-                id: id.to_string(),
-                error: FrontendRpcErrorData {
-                    code: JsonRpcErrorCode::InternalError,
-                    message: err.to_string(),
-                },
-            }),
-        };
+    //     // Convert the reply to a message we can send to the front end
+    //     let reply = match result {
+    //         Ok(value) => FrontendMessage::RpcResultResponse(FrontendRpcResult {
+    //             id: id.to_string(),
+    //             result: value,
+    //         }),
+    //         Err(err) => FrontendMessage::RpcResultError(FrontendRpcError {
+    //             id: id.to_string(),
+    //             error: FrontendRpcErrorData {
+    //                 code: JsonRpcErrorCode::InternalError,
+    //                 message: err.to_string(),
+    //             },
+    //         }),
+    //     };
 
-        let comm_msg = CommMsg::Rpc(id.to_string(), serde_json::to_value(reply)?);
+    //     let comm_msg = CommMsg::Rpc(id.to_string(), serde_json::to_value(reply)?);
 
-        // Deliver the RPC reply to the front end over the comm channel
-        if let Err(err) = self.comm.outgoing_tx.send(comm_msg) {
-            log::error!("Error sending Positron event to front end: {}", err);
-        };
-        Ok(())
-    }
+    //     // Deliver the RPC reply to the front end over the comm channel
+    //     if let Err(err) = self.comm.outgoing_tx.send(comm_msg) {
+    //         log::error!("Error sending Positron event to front end: {}", err);
+    //     };
+    //     Ok(())
+    // }
 }
