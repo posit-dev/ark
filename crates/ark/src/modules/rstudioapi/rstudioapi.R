dff73bbb5b9cb6881581c2b1bda4b50d76c1f786
Jennifer (Jenny) Bryan <jenny.f.bryan@gmail.com>
2024-01-24 23:14:14 -0800

    This needs to be a function (#212)

--- crates/ark/src/modules/rstudioapi/rstudioapi.R
+++ crates/ark/src/modules/rstudioapi/rstudioapi.R
@@ -1,7 +1,20 @@
 #' @export
-.rs.api.versionInfo <- list(
-    citation = NULL,
-    mode = "desktop",
-    version = package_version("2023.3"),
-    long_version = "2023.03.0"
-)
+.rs.api.versionInfo <- function() {
+    # comments are what rstudioapi actually does
+    # https://github.com/rstudio/rstudio/blob/bb729e14867f6f95e26600d8b38e4551402cf7de/src/cpp/r/R/Api.R#L135-L145
+    # info <- list()
+    list(
+        # info$citation <- .Call("rs_rstudioCitation", PACKAGE = "(embedding)")
+        citation = NULL,
+        # info$mode <- .Call("rs_rstudioProgramMode", PACKAGE = "(embedding)")
+        mode = "desktop",
+        # info$edition <- .Call("rs_rstudioEdition", PACKAGE = "(embedding)")
+        # info$version <- .Call("rs_rstudioVersion", PACKAGE = "(embedding)")
+        # info$version <- package_version(info$version)
+        version = package_version("2023.3"),
+        # info$long_version <- .Call("rs_rstudioLongVersion", PACKAGE = "(embedding)")
+        long_version = "2023.03.0"
+        # info$release_name <- .Call("rs_rstudioReleaseName", PACKAGE = "(embedding)")
+    )
+    #     info
+}
