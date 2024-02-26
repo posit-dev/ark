9bbfcf24881b6dd45f5000320e7f7513227c20d2
Daniel Falbel <dfalbel@gmail.com>
2024-02-26 17:08:15 -0300

    Prefer depth-first search.

--- crates/ark/src/modules/positron/connection.R
+++ crates/ark/src/modules/positron/connection.R
@@ -84,7 +84,7 @@ connection_flatten_object_types <- function(object_tree) {
         object_tree <- object_tree[-1]
         if (!is.null(object$contains) && !identical(object$contains, "data")) {
             contains <- object$contains[!names(object$contains) %in% names(object_types)]
-            object_tree <- c(object_tree, contains)
+            object_tree <- c(contains, object_tree)
         }
     }
     object_types
