34c737082960b8fb4e01b7b97a350c2697f92cf4
Davis Vaughan <davis@rstudio.com>
2024-02-28 11:31:27 -0500

    Regenerate comms with `@generated` tags (#241)
    * `format_generated_files = false`
    
    * Regenerate
    
    * Manually skip formatting generated comm files

--- crates/amalthea/src/comm/mod.rs
+++ crates/amalthea/src/comm/mod.rs
@@ -5,13 +5,22 @@
  *
  */
 
+// Until rustfmt respects `@generated` when executing Format on Save, we have to manually
+// skip generated comm files to avoid noise while debugging them
+// https://github.com/rust-lang/rustfmt/issues/5080
+
 pub mod base_comm;
 pub mod comm_channel;
 pub mod comm_manager;
+#[rustfmt::skip]
 pub mod data_explorer_comm;
 pub mod event;
+#[rustfmt::skip]
 pub mod help_comm;
+#[rustfmt::skip]
 pub mod plot_comm;
 pub mod server_comm;
+#[rustfmt::skip]
 pub mod ui_comm;
+#[rustfmt::skip]
 pub mod variables_comm;
