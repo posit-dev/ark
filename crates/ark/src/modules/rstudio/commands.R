cc490436151c9f577580544aee4fe8e055e7582a
Julia Silge <julia.silge@gmail.com>
2024-02-28 20:20:03 -0700

    We can of course do more

--- crates/ark/src/modules/rstudio/commands.R
+++ crates/ark/src/modules/rstudio/commands.R
@@ -3,6 +3,8 @@
     commandId <- switch(
         commandId,
         "activateConsole" = "workbench.action.positronConsole.focusConsole",
+        "activateTerminal" = "workbench.action.terminal.focus",
+        "saveAllSourceDocs" = "workbench.action.files.saveAll",
         {
             if (!quiet) stop("This command is not yet supported in Positron.")
             return()
