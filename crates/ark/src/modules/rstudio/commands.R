cd2cc126eaa365d7bc0df5cd377bc67f66bd5803
Julia Silge <julia.silge@gmail.com>
2024-03-11 20:44:12 -0600

    Add more RStudio commands

--- crates/ark/src/modules/rstudio/commands.R
+++ crates/ark/src/modules/rstudio/commands.R
@@ -5,6 +5,8 @@
         "activateConsole" = "workbench.action.positronConsole.focusConsole",
         "activateTerminal" = "workbench.action.terminal.focus",
         "saveAllSourceDocs" = "workbench.action.files.saveAll",
+        "vcsRefresh" = "git.refresh",
+        "refreshFiles" = "workbench.files.action.refreshFilesExplorer",
         {
             if (!quiet) {
                 .ps.ui.showMessage(paste0("The command '", commandId, "' does not exist."))
