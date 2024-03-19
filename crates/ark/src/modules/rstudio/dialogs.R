f079a92a2b5ef2e81d5e18709bb9252c483d5312
Julia Silge <julia.silge@gmail.com>
2024-03-19 11:12:35 -0600

    Add OpenRPC method for `show_dialog` (#269)
    * Add OpenRPC method for `show_dialog`
    
    * Bump ark to 0.1.66

--- crates/ark/src/modules/rstudio/dialogs.R
+++ crates/ark/src/modules/rstudio/dialogs.R
@@ -1,4 +1,10 @@
 #' @export
+.rs.api.showDialog <- function(title, message, url = "") {
+    stopifnot(url == "")
+    .ps.ui.showDialog(title, message)
+}
+
+#' @export
 .rs.api.showQuestion <- function(title, message, ok = NULL, cancel = NULL) {
     .ps.ui.showQuestion(title, message, ok, cancel)
 }
