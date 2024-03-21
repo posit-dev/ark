216232d46c8b3a1dfd3b072dc4a648362ac17def
Julia Silge <julia.silge@gmail.com>
2024-03-21 12:24:37 -0600

    Handle `url` arg for `rstudioapi::showDialog()` (#271)
    * Handle `url` arg for `rstudioapi::showDialog()`
    
    * Bump ark version
    
    * Avoid leaving an extra <br> in the `message`
    
    * Bump ark to 0.1.69

--- crates/ark/src/modules/rstudio/dialogs.R
+++ crates/ark/src/modules/rstudio/dialogs.R
@@ -1,6 +1,9 @@
 #' @export
 .rs.api.showDialog <- function(title, message, url = "") {
-    stopifnot(url == "")
+    if (!identical(url, "")) {
+        url <- sprintf('<a href="%s">%s</a>', url, url)
+        message <- sprintf('%s<br>%s', message, url)
+    }
     .ps.ui.showDialog(title, message)
 }
 
