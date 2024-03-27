bc8ebccfbc59f770df3d90e07979d63b3c22ef78
Jonathan McPherson <jonathan@rstudio.com>
2024-03-27 11:24:06 -0700

    add a test for ensuring data is returned

--- crates/ark/tests/data_explorer.rs
+++ crates/ark/tests/data_explorer.rs
@@ -8,8 +8,10 @@
 use amalthea::comm::comm_channel::CommMsg;
 use amalthea::comm::data_explorer_comm::DataExplorerBackendReply;
 use amalthea::comm::data_explorer_comm::DataExplorerBackendRequest;
+use amalthea::comm::data_explorer_comm::GetDataValuesParams;
 use amalthea::comm::data_explorer_comm::GetSchemaParams;
 use amalthea::comm::event::CommManagerEvent;
+use amalthea::socket;
 use ark::data_explorer::r_data_explorer::RDataExplorer;
 use ark::r_task;
 use crossbeam::channel::bounded;
@@ -18,6 +20,36 @@ use harp::exec::RFunctionExt;
 use harp::test::start_r;
 use harp::utils::r_envir_get;
 use libr::R_GlobalEnv;
+
+/// Helper method for sending a request to the data explorer and receiving a reply.
+///
+/// Parameters:
+/// - socket: The comm socket to use for communication.
+/// - req: The request to send.
+fn socket_rpc(
+    socket: &socket::comm::CommSocket,
+    req: DataExplorerBackendRequest,
+) -> DataExplorerBackendReply {
+    // Randomly generate a unique ID for this request.
+    let id = uuid::Uuid::new_v4().to_string();
+
+    // Covnert the request to a CommMsg and send it.
+    let msg = CommMsg::Rpc(String::from(id), serde_json::to_value(req).unwrap());
+    socket.incoming_tx.send(msg).unwrap();
+    let msg = socket
+        .outgoing_rx
+        .recv_timeout(std::time::Duration::from_secs(1))
+        .unwrap();
+
+    // Extract the reply from the CommMsg.
+    match msg {
+        CommMsg::Rpc(_id, value) => {
+            let reply: DataExplorerBackendReply = serde_json::from_value(value).unwrap();
+            reply
+        },
+        _ => panic!("Unexpected Comm Message"),
+    }
+}
 
 #[test]
 fn test_data_explorer() {
@@ -57,24 +89,27 @@ fn test_data_explorer() {
         num_columns: 11,
         start_index: 0,
     });
-    let msg = CommMsg::Rpc(String::from("schema-1"), serde_json::to_value(req).unwrap());
-    socket.incoming_tx.send(msg).unwrap();
-
-    // Wait for the reply.
-    let msg = socket
-        .outgoing_rx
-        .recv_timeout(std::time::Duration::from_secs(1))
-        .unwrap();
-    match msg {
-        CommMsg::Rpc(_id, value) => {
-            let reply: DataExplorerBackendReply = serde_json::from_value(value).unwrap();
-            match reply {
-                DataExplorerBackendReply::GetSchemaReply(schema) => {
-                    assert_eq!(schema.columns.len(), 11);
-                },
-                _ => panic!("Unexpected Data Explorer Reply: {:?}", reply),
-            }
+    let reply = socket_rpc(&socket, req);
+    match reply {
+        DataExplorerBackendReply::GetSchemaReply(schema) => {
+            // mtcars is a data frame with 11 columns, so we should get
+            // 11 columns back.
+            assert_eq!(schema.columns.len(), 11);
         },
-        _ => panic!("Unexpected Comm Message"),
+        _ => panic!("Unexpected Data Explorer Reply: {:?}", reply),
+    }
+
+    // Get 5 rows of data from the middle of the test data set.
+    let req = DataExplorerBackendRequest::GetDataValues(GetDataValuesParams {
+        row_start_index: 5,
+        num_rows: 5,
+        column_indices: vec![0, 1, 2, 3, 4],
+    });
+    let reply = socket_rpc(&socket, req);
+    match reply {
+        DataExplorerBackendReply::GetDataValuesReply(data) => {
+            assert_eq!(data.columns.len(), 5);
+        },
+        _ => panic!("Unexpected Data Explorer Reply: {:?}", reply),
     }
 }
