30b3d5c5955d6d98d9a35747dede60e006b7c354
Davis Vaughan <davis@rstudio.com>
2024-03-28 12:38:36 -0400

    Use `saveAllTitled` in `.rs.api.documentSaveAll()` (#279)
    * Use `saveAllTitled` in `.rs.api.documentSaveAll()`
    
    * Apply suggestions from code review
    
    Co-authored-by: Julia Silge <julia.silge@gmail.com>
    
    ---------
    
    Co-authored-by: Julia Silge <julia.silge@gmail.com>

--- crates/ark/src/modules/rstudio/document-api.R
+++ crates/ark/src/modules/rstudio/document-api.R
@@ -62,7 +62,8 @@ convert_position <- function(ps_pos) {
 
 #' @export
 .rs.api.documentSaveAll <- function() {
-    invisible(.ps.ui.executeCommand("workbench.action.files.saveAll"))
+    # This function excludes untitled files in RStudio:
+    invisible(.ps.ui.executeCommand("workbench.action.files.saveAllTitled"))
 }
 
 #' @export
