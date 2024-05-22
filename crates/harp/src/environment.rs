04946a2b868582e13ef3ad31409b9961a85f142d
Lionel Henry <lionel.hry@gmail.com>
2024-05-22 08:27:36 -0400

    Simplify environment iterator
    Remove dependencies on R internals

--- crates/harp/src/environment.rs
+++ crates/harp/src/environment.rs
@@ -11,29 +11,30 @@ use libr::*;
 use once_cell::sync::Lazy;
 use stdext::unwrap;
 
+pub use crate::environment_iter::*;
 use crate::exec::RFunction;
 use crate::exec::RFunctionExt;
 use crate::object::RObject;
 use crate::r_symbol;
 use crate::symbol::RSymbol;
-use crate::utils::r_is_altrep;
-use crate::utils::r_is_null;
-use crate::utils::r_is_s4;
-use crate::utils::r_typeof;
-use crate::utils::Sxpinfo;
 
 const FRAME_LOCK_MASK: std::ffi::c_int = 1 << 14;
+
+#[derive(Clone)]
+pub struct Environment {
+    pub env: RObject,
+}
+
+pub enum EnvironmentFilter {
+    IncludeHiddenBindings,
+    ExcludeHiddenBindings,
+}
 
 pub struct REnvs {
     pub global: SEXP,
     pub base: SEXP,
     pub empty: SEXP,
 }
-
-// Silences diagnostics when called from `r_task()`. Should only be
-// accessed from the R thread.
-unsafe impl Send for REnvs {}
-unsafe impl Sync for REnvs {}
 
 pub static R_ENVS: Lazy<REnvs> = Lazy::new(|| unsafe {
     REnvs {
@@ -42,326 +43,6 @@ pub static R_ENVS: Lazy<REnvs> = Lazy::new(|| unsafe {
         empty: R_EmptyEnv,
     }
 });
-
-#[derive(Eq)]
-pub struct BindingReference {
-    pub reference: bool,
-}
-
-fn has_reference(value: SEXP) -> bool {
-    if r_is_null(value) {
-        return false;
-    }
-
-    if r_is_altrep(value) {
-        unsafe {
-            return has_reference(R_altrep_data1(value)) || has_reference(R_altrep_data2(value));
-        }
-    }
-
-    unsafe {
-        // S4 slots are attributes and might be expandable
-        // so we need to check if they have reference objects
-        if r_is_s4(value) && has_reference(ATTRIB(value)) {
-            return true;
-        }
-    }
-
-    let rtype = r_typeof(value);
-    match rtype {
-        ENVSXP => true,
-
-        LISTSXP | LANGSXP => unsafe { has_reference(CAR(value)) || has_reference(CDR(value)) },
-
-        VECSXP | EXPRSXP => unsafe {
-            let n = Rf_xlength(value);
-            let mut has_ref = false;
-            for i in 0..n {
-                if has_reference(VECTOR_ELT(value, i)) {
-                    has_ref = true;
-                    break;
-                }
-            }
-            has_ref
-        },
-
-        _ => false,
-    }
-}
-
-impl BindingReference {
-    fn new(value: SEXP) -> Self {
-        Self {
-            reference: has_reference(value),
-        }
-    }
-}
-
-impl PartialEq for BindingReference {
-    fn eq(&self, other: &Self) -> bool {
-        !(self.reference || other.reference)
-    }
-}
-
-#[derive(Eq, PartialEq)]
-pub enum BindingValue {
-    Active {
-        fun: RObject,
-    },
-    Promise {
-        promise: RObject,
-    },
-    Altrep {
-        object: RObject,
-        data1: RObject,
-        data2: RObject,
-        reference: BindingReference,
-    },
-    Standard {
-        object: RObject,
-        reference: BindingReference,
-    },
-}
-
-#[derive(Eq, PartialEq)]
-pub struct Binding {
-    pub name: RSymbol,
-    pub value: BindingValue,
-}
-
-impl Binding {
-    pub fn new(env: SEXP, frame: SEXP) -> Self {
-        unsafe {
-            let name = RSymbol::new_unchecked(TAG(frame));
-
-            let info = Sxpinfo::interpret(&frame);
-
-            if info.is_immediate() {
-                // force the immediate bindings before we can safely call CAR()
-                Rf_findVarInFrame(env, *name);
-            }
-            let mut value = CAR(frame);
-
-            if info.is_active() {
-                let value = BindingValue::Active {
-                    fun: RObject::from(value),
-                };
-                return Self { name, value };
-            }
-
-            if r_typeof(value) == PROMSXP {
-                let pr_value = PRVALUE(value);
-                if pr_value == R_UnboundValue {
-                    let code = PRCODE(value);
-                    match r_typeof(code) {
-                        // only consider calls and symbols to be promises
-                        LANGSXP | SYMSXP => {
-                            let value = BindingValue::Promise {
-                                promise: RObject::from(value),
-                            };
-                            return Self { name, value };
-                        },
-                        // all other types are not regarded as promises
-                        // but rather as their underlying object
-                        _ => {
-                            value = code;
-                        },
-                    }
-                } else {
-                    value = pr_value;
-                }
-            }
-
-            if r_is_altrep(value) {
-                let value = BindingValue::Altrep {
-                    object: RObject::from(value),
-                    data1: RObject::from(R_altrep_data1(value)),
-                    data2: RObject::from(R_altrep_data2(value)),
-                    reference: BindingReference::new(value),
-                };
-                return Self { name, value };
-            }
-
-            let value = BindingValue::Standard {
-                object: RObject::from(value),
-                reference: BindingReference::new(value),
-            };
-            Self { name, value }
-        }
-    }
-
-    pub fn is_hidden(&self) -> bool {
-        String::from(self.name).starts_with(".")
-    }
-
-    pub fn is_active(&self) -> bool {
-        if let BindingValue::Active { .. } = self.value {
-            true
-        } else {
-            false
-        }
-    }
-}
-
-#[derive(Clone, Debug)]
-pub struct Environment {
-    env: RObject,
-}
-
-impl Deref for Environment {
-    type Target = SEXP;
-    fn deref(&self) -> &Self::Target {
-        &self.env.sexp
-    }
-}
-
-pub struct HashedEnvironmentIter<'a> {
-    env: &'a Environment,
-
-    hashtab: SEXP,
-    hashtab_index: R_xlen_t,
-    frame: SEXP,
-}
-
-impl<'a> HashedEnvironmentIter<'a> {
-    pub fn new(env: &'a Environment) -> Self {
-        unsafe {
-            let hashtab = HASHTAB(**env);
-            let hashtab_len = Rf_xlength(hashtab);
-            let mut hashtab_index = 0;
-            let mut frame = R_NilValue;
-
-            // look for the first non null frame
-            loop {
-                let f = VECTOR_ELT(hashtab, hashtab_index);
-                if f != R_NilValue {
-                    frame = f;
-                    break;
-                }
-
-                hashtab_index = hashtab_index + 1;
-                if hashtab_index == hashtab_len {
-                    break;
-                }
-            }
-
-            Self {
-                env,
-                hashtab,
-                hashtab_index,
-                frame,
-            }
-        }
-    }
-}
-
-impl<'a> Iterator for HashedEnvironmentIter<'a> {
-    type Item = Binding;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        unsafe {
-            if self.frame == R_NilValue {
-                return None;
-            }
-
-            // grab the next Binding
-            let binding = Binding::new(*self.env.env, self.frame);
-
-            // and advance to next binding
-            self.frame = CDR(self.frame);
-
-            if self.frame == R_NilValue {
-                // end of frame: move to the next non empty frame
-                let hashtab_len = Rf_xlength(self.hashtab);
-                loop {
-                    // move to the next frame
-                    self.hashtab_index = self.hashtab_index + 1;
-
-                    // end of iteration
-                    if self.hashtab_index == hashtab_len {
-                        self.frame = R_NilValue;
-                        break;
-                    }
-
-                    // skip empty frames
-                    self.frame = VECTOR_ELT(self.hashtab, self.hashtab_index);
-                    if self.frame != R_NilValue {
-                        break;
-                    }
-                }
-            }
-
-            Some(binding)
-        }
-    }
-}
-
-pub struct NonHashedEnvironmentIter<'a> {
-    env: &'a Environment,
-
-    frame: SEXP,
-}
-
-impl<'a> NonHashedEnvironmentIter<'a> {
-    pub fn new(env: &'a Environment) -> Self {
-        unsafe {
-            Self {
-                env,
-                frame: FRAME(**env),
-            }
-        }
-    }
-}
-
-impl<'a> Iterator for NonHashedEnvironmentIter<'a> {
-    type Item = Binding;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        unsafe {
-            if self.frame == R_NilValue {
-                None
-            } else {
-                let binding = Binding::new(*self.env.env, self.frame);
-                self.frame = CDR(self.frame);
-                Some(binding)
-            }
-        }
-    }
-}
-
-pub enum EnvironmentIter<'a> {
-    Hashed(HashedEnvironmentIter<'a>),
-    NonHashed(NonHashedEnvironmentIter<'a>),
-}
-
-impl<'a> EnvironmentIter<'a> {
-    pub fn new(env: &'a Environment) -> Self {
-        unsafe {
-            let hashtab = HASHTAB(**env);
-            if hashtab == R_NilValue {
-                EnvironmentIter::NonHashed(NonHashedEnvironmentIter::new(env))
-            } else {
-                EnvironmentIter::Hashed(HashedEnvironmentIter::new(env))
-            }
-        }
-    }
-}
-
-pub enum EnvironmentFilter {
-    IncludeHiddenBindings,
-    ExcludeHiddenBindings,
-}
-
-impl<'a> Iterator for EnvironmentIter<'a> {
-    type Item = Binding;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        match self {
-            EnvironmentIter::Hashed(iter) => iter.next(),
-            EnvironmentIter::NonHashed(iter) => iter.next(),
-        }
-    }
-}
 
 impl Environment {
     pub fn new(env: RObject) -> Self {
@@ -396,26 +77,11 @@ impl Environment {
     }
 
     pub fn iter(&self) -> EnvironmentIter {
-        EnvironmentIter::new(&self)
+        EnvironmentIter::new(self.clone())
     }
 
     pub fn exists(&self, name: impl Into<RSymbol>) -> bool {
-        let name = name.into();
-        match self.iter() {
-            EnvironmentIter::Hashed(mut iter) => {
-                /* TODO: we could eventually only iterate in the frame with the right hash
-
-                // does the symbol know its hash:
-                let has_hash = name.has_hash();
-
-                hashcode = HASHVALUE(PRINTNAME(TAG(frame))) % HASHSIZE(table)
-                chain = VECTOR_ELT(table, hashcode);
-
-                */
-                iter.find(|b| b.name == name).is_some()
-            },
-            EnvironmentIter::NonHashed(mut iter) => iter.find(|b| b.name == name).is_some(),
-        }
+        unsafe { libr::R_existsVarInFrame(self.env.sexp, name.into().sexp) != 0 }
     }
 
     pub fn find(&self, name: impl Into<RSymbol>) -> SEXP {
@@ -495,8 +161,19 @@ impl Environment {
         unsafe { libr::R_EnvironmentIsLocked(self.env.sexp) != 0 }
     }
 
+    pub fn is_active(&self, name: RSymbol) -> bool {
+        unsafe { libr::R_BindingIsActive(name.sexp, self.env.sexp) != 0 }
+    }
+
     fn flags(&self) -> std::ffi::c_int {
         unsafe { libr::ENVFLAGS(self.env.sexp) }
+    }
+}
+
+impl Deref for Environment {
+    type Target = SEXP;
+    fn deref(&self) -> &Self::Target {
+        &self.env.sexp
     }
 }
 
@@ -512,52 +189,16 @@ impl From<Environment> for RObject {
     }
 }
 
+// Silences diagnostics when called from `r_task()`. Should only be
+// accessed from the R thread.
+unsafe impl Send for REnvs {}
+unsafe impl Sync for REnvs {}
+
 #[harp::register]
 pub extern "C" fn ark_env_unlock(env: SEXP) -> crate::error::Result<SEXP> {
     unsafe {
         crate::check_env(env)?;
         Environment::view(env).unlock();
         Ok(libr::R_NilValue)
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use libr::Rf_ScalarInteger;
-    use libr::Rf_defineVar;
-
-    use super::*;
-    use crate::exec::RFunction;
-    use crate::exec::RFunctionExt;
-    use crate::r_symbol;
-    use crate::r_test;
-
-    unsafe fn test_environment_iter_impl(hash: bool) {
-        let test_env = RFunction::new("base", "new.env")
-            .param("parent", R_EmptyEnv)
-            .param("hash", RObject::from(hash))
-            .call()
-            .unwrap();
-
-        let sym = r_symbol!("a");
-        Rf_defineVar(sym, Rf_ScalarInteger(42), test_env.sexp);
-
-        let sym = r_symbol!("b");
-        Rf_defineVar(sym, Rf_ScalarInteger(43), test_env.sexp);
-
-        let sym = r_symbol!("c");
-        Rf_defineVar(sym, Rf_ScalarInteger(44), test_env.sexp);
-
-        let env = Environment::new(test_env);
-        assert_eq!(env.iter().count(), 3);
-    }
-
-    #[test]
-    #[allow(non_snake_case)]
-    fn test_environment_iter() {
-        r_test! {
-            test_environment_iter_impl(true);
-            test_environment_iter_impl(false);
-        }
     }
 }
