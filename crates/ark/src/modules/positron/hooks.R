9f2d72302e7d5c9a9585aeb03131262d0c15dcd6
Davis Vaughan <davis@posit.co>
2024-06-06 12:10:39 -0400

    Override `try()` to force `show.error.messages = TRUE` locally
    This is essentially the same approach we take while in the debugger, which is also a place where we aren't given an opportunity to capture error conditions, but `show.error.messages = FALSE` is still allowed in.

--- crates/ark/src/modules/positron/hooks.R
+++ crates/ark/src/modules/positron/hooks.R
@@ -29,8 +29,64 @@
     }
 }
 
+# TODO: Merge https://github.com/posit-dev/amalthea/pull/383 first
+# and use the utilities there to make this cleaner
+#' @export
+.ps.register_base_hook <- function(name, hook, namespace = FALSE) {
+    package_name <- "package:base"
+    # check if function exists
+    if (!exists(name, package_name, mode = "function")) {
+        msg <- sprintf("Could not register hook: function `base::%s` not found", name)
+        stop(msg, call. = FALSE)
+    }
+    # replaces the unnamespaced, attached version of the function
+    utils_env <- as.environment(package_name)
+    env_bind_force(utils_env, name, hook)
+
+    # if namespace = TRUE, we try to replace the binding for `base::name` as well
+    if (namespace) {
+        utils_ns <- asNamespace("base")
+        if (!exists(name, utils_ns, mode = "function")) {
+            msg <- sprintf("Could not replace `%s` in the `base` namespace", name)
+            warning(msg, call. = FALSE)
+        } else {
+            env_bind_force(utils_ns, name, hook)
+        }
+    }
+}
+
+# `try()` is the one other place in base R besides `stop()` that utilizes
+# `show.error.messages`. We forcibly set `show.error.messages = FALSE` when
+# initializing our global calling handler for errors, which causes `try()` to
+# always act like `silent = TRUE` is set. Unlike errors that go through `stop()`,
+# `try()` does not offer any other way for us to capture the error output and
+# relay it to the user ourselves (like through global calling handlers). Our
+# best solution for this is to override `try()` to locally force
+# `show.error.messages` back to `TRUE`. This prevents the user from tweaking
+# this option, but we don't expect them to do that anyways.
+.ps.register_try_hook <- function() {
+    # Can't use `local_options()` in the body, must be base code
+    body <- body(base::try)
+    body <- bquote({
+        old <- options(show.error.messages = TRUE)
+        on.exit(options(old), add = TRUE, after = FALSE)
+        .(body)
+    })
+
+    # Keep signature in sync with `try()`
+    fn <- function(
+        expr,
+        silent = FALSE,
+        outFile = getOption("try.outFile", default = stderr())
+    ) {}
+    body(fn) <- body
+
+    .ps.register_base_hook("try", fn, namespace = TRUE)
+}
+
 # Wrapper to contain the definition of all hooks we want to register
 #' @export
 .ps.register_all_hooks <- function() {
     .ps.register_utils_hook("View", .ps.view_data_frame, namespace = TRUE)
+    .ps.register_try_hook()
 }
