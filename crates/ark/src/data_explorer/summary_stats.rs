2d9a5e84ae945480fde1764fa6b0a859088e9cb6
Daniel Falbel <dfalbel@gmail.com>
2024-06-17 12:27:00 -0300

    Simplify code to avoid using macro

--- crates/ark/src/data_explorer/summary_stats.rs
+++ crates/ark/src/data_explorer/summary_stats.rs
@@ -11,6 +11,11 @@ use amalthea::comm::data_explorer_comm;
 use amalthea::comm::data_explorer_comm::ColumnDisplayType;
 use amalthea::comm::data_explorer_comm::ColumnSummaryStats;
 use amalthea::comm::data_explorer_comm::FormatOptions;
+use amalthea::comm::data_explorer_comm::SummaryStatsBoolean;
+use amalthea::comm::data_explorer_comm::SummaryStatsDate;
+use amalthea::comm::data_explorer_comm::SummaryStatsDatetime;
+use amalthea::comm::data_explorer_comm::SummaryStatsNumber;
+use amalthea::comm::data_explorer_comm::SummaryStatsString;
 use anyhow::anyhow;
 use harp::exec::RFunction;
 use harp::exec::RFunctionExt;
@@ -45,14 +50,25 @@ fn summary_stats_(
     display_type: ColumnDisplayType,
     format_options: &FormatOptions,
 ) -> anyhow::Result<ColumnSummaryStats> {
-    match display_type {
-        ColumnDisplayType::Number => Ok(summary_stats_number(column, format_options)?.into()),
-        ColumnDisplayType::String => Ok(summary_stats_string(column)?.into()),
-        ColumnDisplayType::Boolean => Ok(summary_stats_boolean(column)?.into()),
-        ColumnDisplayType::Date => Ok(summary_stats_date(column)?.into()),
-        ColumnDisplayType::Datetime => Ok(summary_stats_datetime(column)?.into()),
-        _ => Err(anyhow::anyhow!("Unkown type")),
-    }
+    let mut stats = empty_column_summary_stats();
+    stats.type_display = display_type;
+    match stats.type_display {
+        ColumnDisplayType::Number => {
+            stats.number_stats = Some(summary_stats_number(column, format_options)?);
+        },
+        ColumnDisplayType::String => {
+            stats.string_stats = Some(summary_stats_string(column)?);
+        },
+        ColumnDisplayType::Boolean => {
+            stats.boolean_stats = Some(summary_stats_boolean(column)?);
+        },
+        ColumnDisplayType::Date => stats.date_stats = Some(summary_stats_date(column)?),
+        ColumnDisplayType::Datetime => stats.datetime_stats = Some(summary_stats_datetime(column)?),
+        _ => {
+            return Err(anyhow::anyhow!("Unkown type"));
+        },
+    };
+    Ok(stats)
 }
 
 fn summary_stats_number(
@@ -73,35 +89,33 @@ fn summary_stats_number(
         })
         .collect();
 
-    Ok(SummaryStatsNumber(data_explorer_comm::SummaryStatsNumber {
+    Ok(SummaryStatsNumber {
         min_value: get_stat(&r_stats, "min_value")?,
         max_value: get_stat(&r_stats, "max_value")?,
         mean: get_stat(&r_stats, "mean")?,
         median: get_stat(&r_stats, "median")?,
         stdev: get_stat(&r_stats, "stdev")?,
-    }))
+    })
 }
 
 fn summary_stats_string(column: SEXP) -> anyhow::Result<SummaryStatsString> {
     let stats = call_summary_fn("summary_stats_string", column)?;
     let r_stats: HashMap<String, i32> = stats.try_into()?;
 
-    Ok(SummaryStatsString(data_explorer_comm::SummaryStatsString {
+    Ok(SummaryStatsString {
         num_empty: get_stat(&r_stats, "num_empty")? as i64,
         num_unique: get_stat(&r_stats, "num_unique")? as i64,
-    }))
+    })
 }
 
 fn summary_stats_boolean(column: SEXP) -> anyhow::Result<SummaryStatsBoolean> {
     let stats = call_summary_fn("summary_stats_boolean", column)?;
     let r_stats: HashMap<String, i32> = stats.try_into()?;
 
-    Ok(SummaryStatsBoolean(
-        data_explorer_comm::SummaryStatsBoolean {
-            true_count: get_stat(&r_stats, "true_count")? as i64,
-            false_count: get_stat(&r_stats, "false_count")? as i64,
-        },
-    ))
+    Ok(SummaryStatsBoolean {
+        true_count: get_stat(&r_stats, "true_count")? as i64,
+        false_count: get_stat(&r_stats, "false_count")? as i64,
+    })
 }
 
 fn summary_stats_date(column: SEXP) -> anyhow::Result<SummaryStatsDate> {
@@ -110,13 +124,13 @@ fn summary_stats_date(column: SEXP) -> anyhow::Result<SummaryStatsDate> {
 
     let num_unique: i32 = get_stat(&r_stats, "num_unique")?.try_into()?;
 
-    Ok(SummaryStatsDate(data_explorer_comm::SummaryStatsDate {
+    Ok(SummaryStatsDate {
         min_date: robj_to_string(&get_stat(&r_stats, "min_date")?),
         mean_date: robj_to_string(&get_stat(&r_stats, "mean_date")?),
         median_date: robj_to_string(&get_stat(&r_stats, "median_date")?),
         max_date: robj_to_string(&get_stat(&r_stats, "max_date")?),
         num_unique: num_unique as i64,
-    }))
+    })
 }
 
 fn summary_stats_datetime(column: SEXP) -> anyhow::Result<SummaryStatsDatetime> {
@@ -131,16 +145,14 @@ fn summary_stats_datetime(column: SEXP) -> anyhow::Result<SummaryStatsDatetime> 
         .call_in(ARK_ENVS.positron_ns)?
         .try_into()?;
 
-    Ok(SummaryStatsDatetime(
-        data_explorer_comm::SummaryStatsDatetime {
-            min_date: robj_to_string(&get_stat(&r_stats, "min_date")?),
-            mean_date: robj_to_string(&get_stat(&r_stats, "mean_date")?),
-            median_date: robj_to_string(&get_stat(&r_stats, "median_date")?),
-            max_date: robj_to_string(&get_stat(&r_stats, "max_date")?),
-            num_unique: num_unique as i64,
-            timezone,
-        },
-    ))
+    Ok(SummaryStatsDatetime {
+        min_date: robj_to_string(&get_stat(&r_stats, "min_date")?),
+        mean_date: robj_to_string(&get_stat(&r_stats, "mean_date")?),
+        median_date: robj_to_string(&get_stat(&r_stats, "median_date")?),
+        max_date: robj_to_string(&get_stat(&r_stats, "max_date")?),
+        num_unique: num_unique as i64,
+        timezone,
+    })
 }
 
 fn robj_to_string(robj: &RObject) -> String {
@@ -163,52 +175,6 @@ fn call_summary_fn(function: &str, column: SEXP) -> anyhow::Result<RObject> {
         .add(column)
         .call_in(ARK_ENVS.positron_ns)?)
 }
-
-macro_rules! impl_summary_stats_conversion {
-    ($name:ident, $summary_stats:ident, $comm_type:ty, $display_type:expr) => {
-        struct $summary_stats(pub $comm_type);
-
-        impl From<$summary_stats> for ColumnSummaryStats {
-            fn from(summary_stats: $summary_stats) -> Self {
-                let mut stats = empty_column_summary_stats();
-                stats.type_display = $display_type;
-                stats.$name = Some(summary_stats.0);
-                stats
-            }
-        }
-    };
-}
-
-impl_summary_stats_conversion!(
-    number_stats,
-    SummaryStatsNumber,
-    data_explorer_comm::SummaryStatsNumber,
-    ColumnDisplayType::Number
-);
-impl_summary_stats_conversion!(
-    string_stats,
-    SummaryStatsString,
-    data_explorer_comm::SummaryStatsString,
-    ColumnDisplayType::String
-);
-impl_summary_stats_conversion!(
-    boolean_stats,
-    SummaryStatsBoolean,
-    data_explorer_comm::SummaryStatsBoolean,
-    ColumnDisplayType::Boolean
-);
-impl_summary_stats_conversion!(
-    date_stats,
-    SummaryStatsDate,
-    data_explorer_comm::SummaryStatsDate,
-    ColumnDisplayType::Date
-);
-impl_summary_stats_conversion!(
-    datetime_stats,
-    SummaryStatsDatetime,
-    data_explorer_comm::SummaryStatsDatetime,
-    ColumnDisplayType::Datetime
-);
 
 fn empty_column_summary_stats() -> data_explorer_comm::ColumnSummaryStats {
     data_explorer_comm::ColumnSummaryStats {
@@ -254,16 +220,14 @@ mod tests {
             let column = r_parse_eval0("c(1,2,3,4,5, NA)", R_ENVS.global).unwrap();
             let stats =
                 summary_stats_(column.sexp, ColumnDisplayType::Number, &default_options()).unwrap();
-            let expected: ColumnSummaryStats =
-                SummaryStatsNumber(data_explorer_comm::SummaryStatsNumber {
-                    min_value: "1.00".to_string(),
-                    max_value: "5.00".to_string(),
-                    mean: "3.00".to_string(),
-                    median: "3.00".to_string(),
-                    stdev: "1.58".to_string(),
-                })
-                .into();
-            assert_eq!(stats, expected);
+            let expected = SummaryStatsNumber {
+                min_value: "1.00".to_string(),
+                max_value: "5.00".to_string(),
+                mean: "3.00".to_string(),
+                median: "3.00".to_string(),
+                stdev: "1.58".to_string(),
+            };
+            assert_eq!(stats.number_stats, Some(expected));
         })
     }
 
@@ -273,13 +237,11 @@ mod tests {
             let column = r_parse_eval0("c('a', 'b', 'c', 'd', '')", R_ENVS.global).unwrap();
             let stats =
                 summary_stats_(column.sexp, ColumnDisplayType::String, &default_options()).unwrap();
-            let expected: ColumnSummaryStats =
-                SummaryStatsString(data_explorer_comm::SummaryStatsString {
-                    num_empty: 1,
-                    num_unique: 5,
-                })
-                .into();
-            assert_eq!(stats, expected);
+            let expected = SummaryStatsString {
+                num_empty: 1,
+                num_unique: 5,
+            };
+            assert_eq!(stats.string_stats, Some(expected));
         })
     }
 
@@ -289,13 +251,11 @@ mod tests {
             let column = r_parse_eval0("c(TRUE, FALSE, TRUE, TRUE, NA)", R_ENVS.global).unwrap();
             let stats = summary_stats_(column.sexp, ColumnDisplayType::Boolean, &default_options())
                 .unwrap();
-            let expected: ColumnSummaryStats =
-                SummaryStatsBoolean(data_explorer_comm::SummaryStatsBoolean {
-                    true_count: 3,
-                    false_count: 1,
-                })
-                .into();
-            assert_eq!(stats, expected);
+            let expected = SummaryStatsBoolean {
+                true_count: 3,
+                false_count: 1,
+            };
+            assert_eq!(stats.boolean_stats, Some(expected));
         })
     }
 
@@ -309,16 +269,14 @@ mod tests {
             .unwrap();
             let stats =
                 summary_stats_(column.sexp, ColumnDisplayType::Date, &default_options()).unwrap();
-            let expected: ColumnSummaryStats =
-                SummaryStatsDate(data_explorer_comm::SummaryStatsDate {
-                    min_date: "2021-01-01".to_string(),
-                    mean_date: "2021-01-02".to_string(),
-                    median_date: "2021-01-02".to_string(),
-                    max_date: "2021-01-04".to_string(),
-                    num_unique: 5,
-                })
-                .into();
-            assert_eq!(stats, expected);
+            let expected = SummaryStatsDate {
+                min_date: "2021-01-01".to_string(),
+                mean_date: "2021-01-02".to_string(),
+                median_date: "2021-01-02".to_string(),
+                max_date: "2021-01-04".to_string(),
+                num_unique: 5,
+            };
+            assert_eq!(stats.date_stats, Some(expected));
         })
     }
 
@@ -333,17 +291,15 @@ mod tests {
             let stats =
                 summary_stats_(column.sexp, ColumnDisplayType::Datetime, &default_options())
                     .unwrap();
-            let expected: ColumnSummaryStats =
-                SummaryStatsDatetime(data_explorer_comm::SummaryStatsDatetime {
-                    num_unique: 2,
-                    min_date: "2015-07-24 23:15:07".to_string(),
-                    mean_date: "2015-07-24 23:15:07".to_string(),
-                    median_date: "2015-07-24 23:15:07".to_string(),
-                    max_date: "2015-07-24 23:15:07".to_string(),
-                    timezone: Some("Japan".to_string()),
-                })
-                .into();
-            assert_eq!(stats, expected);
+            let expected = SummaryStatsDatetime {
+                num_unique: 2,
+                min_date: "2015-07-24 23:15:07".to_string(),
+                mean_date: "2015-07-24 23:15:07".to_string(),
+                median_date: "2015-07-24 23:15:07".to_string(),
+                max_date: "2015-07-24 23:15:07".to_string(),
+                timezone: Some("Japan".to_string()),
+            };
+            assert_eq!(stats.datetime_stats, Some(expected));
         })
     }
 }
