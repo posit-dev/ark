c240e11ac5d56a6fee33d930f33dc85a7be453a9
Davis Vaughan <davis@posit.co>
2024-07-16 17:17:18 -0400

    Add default argument values to the signature label

--- crates/ark/src/lsp/signature_help.rs
+++ crates/ark/src/lsp/signature_help.rs
@@ -1,14 +1,25 @@
 //
 // signature.rs
 //
-// Copyright (C) 2022 Posit Software, PBC. All rights reserved.
+// Copyright (C) 2022-2024 Posit Software, PBC. All rights reserved.
 //
 //
 
+use harp::call::r_expr_deparse;
 use harp::eval::r_parse_eval;
 use harp::eval::RParseEvalOptions;
+use harp::object::*;
+use harp::pretty::*;
+use harp::r_missing;
+use harp::r_null;
 use harp::utils::r_formals;
 use harp::utils::r_is_function;
+use harp::utils::r_is_object;
+use harp::utils::r_type2char;
+use harp::utils::r_typeof;
+use harp::utils::sym_quote_invalid;
+use harp::RSymbol;
+use libr::*;
 use log::info;
 use stdext::unwrap;
 use stdext::unwrap::IntoResult;
@@ -223,24 +234,29 @@ pub(crate) unsafe fn r_signature_help(
 
     // Iterate over the documentation for each parameter, and add the relevant information.
     for (index, argument) in formals.iter().enumerate() {
+        // Get argument components.
+        let argument_name = &argument.name;
+        let argument_value = &argument.value;
+        let argument_label = argument_label(argument_name.clone(), argument_value.sexp);
+
         // Compute signature offsets.
         let start = label.len() as u32;
-        let end = start + argument.name.len() as u32;
+        let end = start + argument_label.len() as u32;
 
-        // Add the parameter to the label.
-        label.push_str(argument.name.as_str());
+        // Add the argument label to the overall label.
+        label.push_str(argument_label.as_str());
         label.push_str(", ");
 
         // If we had an explicit name, and this name matches the argument,
         // then update the offset now.
-        if active_argument.as_ref() == Some(&argument.name) {
+        if active_argument.as_ref() == Some(argument_name) {
             offset = Some(index as u32);
         }
 
         // Get documentation, if any.
         let mut documentation = None;
         if let Ok(Some(ref help)) = help {
-            let markup = help.parameter(&argument.name);
+            let markup = help.parameter(argument_name);
             if let Ok(Some(markup)) = markup {
                 documentation = Some(Documentation::MarkupContent(markup));
             }
@@ -336,13 +352,155 @@ fn is_within_call_parentheses(x: &Point, node: &Node) -> bool {
     x.is_after_or_equal(open.end_position()) && x.is_before_or_equal(close.start_position())
 }
 
+fn argument_label(name: String, value: SEXP) -> String {
+    // Specially handle `R_MissingArg`, which looks like a `SYMSXP`,
+    // but we don't want to add `=` to it. This is what we see when
+    // there is no default argument (and also for `...`).
+    if value == r_missing() {
+        return name;
+    }
+
+    name + " = " + obj_label(value).as_str()
+}
+
+fn obj_label(x: SEXP) -> String {
+    if r_is_object(x) {
+        // Just showing the class name for inlined complex objects
+        return s3_label(x);
+    }
+    if r_dim(x) != r_null() {
+        // In the odd case of an inlined array, just show type name
+        return type_label(x);
+    }
+
+    // Most cases are either:
+    // - `NULL`
+    // - Call
+    // - Symbol
+    // - Scalar objects that get inlined automatically
+    // However, it is possible to have more complex arguments inlined into the function
+    // signature, like vctrs does with `fn_inline_formals()`. We also handle most of
+    // those cases well.
+    match r_typeof(x) {
+        NILSXP => null_label(),
+        LANGSXP => call_label(x),
+        SYMSXP => sym_label(x),
+        LGLSXP => vec_label(x, lgl_to_pretty_string),
+        INTSXP => vec_label(x, int_to_pretty_string),
+        REALSXP => vec_label(x, dbl_to_pretty_string),
+        CPLXSXP => vec_label(x, cpl_to_pretty_string),
+        STRSXP => vec_label(x, chr_to_pretty_string),
+        VECSXP => list_label(),
+        _ => return type_label(x),
+    }
+}
+
+fn type_label(x: SEXP) -> String {
+    let out = r_type2char(r_typeof(x));
+    let out = String::from("<") + out.as_str() + ">";
+    out
+}
+
+fn s3_label(x: SEXP) -> String {
+    match r_s3_pretty_class(x) {
+        Ok(class) => class,
+        Err(err) => {
+            log::error!("{err:?}");
+            String::from("<???>")
+        },
+    }
+}
+
+fn null_label() -> String {
+    r_null_to_pretty_string()
+}
+
+fn list_label() -> String {
+    // TODO: Should we try and do better? I doubt an inlined list is that common.
+    String::from("<list>")
+}
+
+fn vec_label(x: SEXP, elt_to_pretty_string: fn(SEXP, isize) -> String) -> String {
+    let size = r_length(x);
+    let scalar = size == 1;
+
+    let mut out = String::from("");
+
+    if !scalar {
+        out.push_str("c(");
+    }
+
+    for i in 0..size {
+        let elt = elt_to_pretty_string(x, i);
+        out.push_str(elt.as_str());
+
+        if i != size - 1 {
+            out.push_str(", ");
+        }
+    }
+
+    if !scalar {
+        out.push_str(")");
+    }
+
+    out
+}
+
+fn lgl_to_pretty_string(x: SEXP, i: isize) -> String {
+    r_lgl_to_pretty_string(r_lgl_get(x, i))
+}
+fn int_to_pretty_string(x: SEXP, i: isize) -> String {
+    r_int_to_pretty_string(r_int_get(x, i))
+}
+fn dbl_to_pretty_string(x: SEXP, i: isize) -> String {
+    r_dbl_to_pretty_string(r_dbl_get(x, i))
+}
+fn cpl_to_pretty_string(x: SEXP, i: isize) -> String {
+    r_cpl_to_pretty_string(r_cpl_get(x, i))
+}
+fn chr_to_pretty_string(x: SEXP, i: isize) -> String {
+    r_str_to_pretty_string(r_chr_get(x, i))
+}
+
+fn sym_label(x: SEXP) -> String {
+    if x == r_missing() {
+        panic!("`R_MissingArg` should have been handled earlier.");
+    }
+
+    let x = RSymbol::new_unchecked(x);
+    let x = String::from(x);
+    let x = sym_quote_invalid(x.as_str());
+
+    x
+}
+
+fn call_label(x: SEXP) -> String {
+    match r_expr_deparse(x) {
+        Ok(x) => x,
+        Err(err) => {
+            log::error!("Can't convert call to text: {err:?}.");
+            String::from("<call>")
+        },
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use harp::call::RCall;
+    use harp::environment::R_ENVS;
+    use harp::eval::r_parse_eval0;
+    use harp::object::*;
+    use harp::r_char;
+    use harp::r_missing;
+    use harp::r_null;
+    use harp::r_symbol;
     use harp::test::r_test;
+    use harp::RObject;
     use tower_lsp::lsp_types::ParameterLabel;
 
     use crate::lsp::document_context::DocumentContext;
     use crate::lsp::documents::Document;
+    use crate::lsp::signature_help::argument_label;
     use crate::lsp::signature_help::r_signature_help;
     use crate::test::point_from_cursor;
 
@@ -380,6 +538,166 @@ mod tests {
             let help = unsafe { r_signature_help(&context) };
             let help = help.unwrap();
             assert!(help.is_none());
+        })
+    }
+
+    #[test]
+    fn test_signature_help_argument_defaults() {
+        r_test(|| {
+            // Define function in global env
+            let fun = r#"
+fn <- function(
+  x,
+  ...,
+  by = NULL,
+  value = 2.5,
+  string = "foo",
+  keep = c("yes", "no", "maybe"),
+  length = c(2L, 3L),
+  call = identity(x),
+  lst = list(1, 2)
+) { }
+"#;
+            r_parse_eval0(fun, R_ENVS.global).unwrap();
+
+            let (text, point) = point_from_cursor("fn(@)");
+            let document = Document::new(&text, None);
+            let context = DocumentContext::new(&document, point, None);
+            let help = unsafe { r_signature_help(&context) };
+            let help = help.unwrap().unwrap();
+
+            // Check expected signature label
+            let signature = help.signatures.get(0).unwrap();
+            assert_eq!(
+                signature.label,
+                String::from(
+                    r#"fn(x, ..., by = NULL, value = 2.5, string = "foo", keep = c("yes", "no", "maybe"), length = c(2L, 3L), call = identity(x), lst = list(1, 2))"#
+                )
+            );
+
+            // Clean up
+            r_parse_eval0("rm(fn)", R_ENVS.global).unwrap();
+        })
+    }
+
+    #[test]
+    fn test_argument_label_null() {
+        r_test(|| {
+            let x = r_null();
+            let label = argument_label(String::from("x"), x);
+            assert_eq!(label, String::from("x = NULL"));
+        })
+    }
+
+    #[test]
+    fn test_argument_label_missing() {
+        r_test(|| {
+            let x = r_missing();
+            let label = argument_label(String::from("x"), x);
+            assert_eq!(label, String::from("x"));
+        })
+    }
+
+    #[test]
+    fn test_argument_label_symbol() {
+        r_test(|| {
+            let x = unsafe { r_symbol!("name") };
+            let label = argument_label(String::from("x"), x);
+            assert_eq!(label, String::from("x = name"));
+
+            let x = unsafe { r_symbol!("hi there") };
+            let label = argument_label(String::from("x"), x);
+            assert_eq!(label, String::from("x = `hi there`"));
+        })
+    }
+
+    #[test]
+    fn test_argument_label_call() {
+        r_test(|| {
+            let x = unsafe {
+                RCall::new(r_symbol!("source"))
+                    .add(r_symbol!("exprs"))
+                    .param("local", r_symbol!("is_local"))
+                    .build()
+            };
+            let label = argument_label(String::from("x"), x.sexp);
+            assert_eq!(label, String::from("x = source(exprs, local = is_local)"));
+        })
+    }
+
+    #[test]
+    fn test_argument_label_vector() {
+        r_test(|| {
+            let x = RObject::from(r_alloc_logical(3));
+            r_lgl_poke(x.sexp, 0, 1);
+            r_lgl_poke(x.sexp, 1, 0);
+            r_lgl_poke(x.sexp, 2, r_lgl_na());
+            let label = argument_label(String::from("x"), x.sexp);
+            assert_eq!(label, String::from("x = c(TRUE, FALSE, NA)"));
+
+            let x = RObject::from(r_alloc_integer(3));
+            r_int_poke(x.sexp, 0, 1);
+            r_int_poke(x.sexp, 1, 2);
+            r_int_poke(x.sexp, 2, r_int_na());
+            let label = argument_label(String::from("x"), x.sexp);
+            assert_eq!(label, String::from("x = c(1L, 2L, NA)"));
+
+            let x = RObject::from(r_alloc_double(3));
+            r_dbl_poke(x.sexp, 0, 1.5);
+            r_dbl_poke(x.sexp, 1, 2.5);
+            r_dbl_poke(x.sexp, 2, r_dbl_na());
+            let label = argument_label(String::from("x"), x.sexp);
+            assert_eq!(label, String::from("x = c(1.5, 2.5, NA)"));
+
+            let x = RObject::from(r_alloc_complex(3));
+            r_cpl_poke(x.sexp, 0, libr::Rcomplex { r: 1.0, i: 2.5 });
+            r_cpl_poke(x.sexp, 1, libr::Rcomplex {
+                r: r_dbl_positive_infinity(),
+                i: r_dbl_negative_infinity(),
+            });
+            r_cpl_poke(x.sexp, 2, libr::Rcomplex {
+                r: r_dbl_na(),
+                i: 2.5,
+            });
+            let label = argument_label(String::from("x"), x.sexp);
+            assert_eq!(label, String::from("x = c(1+2.5i, Inf-Infi, NA+2.5i)"));
+
+            let x = RObject::from(r_alloc_character(3));
+            r_chr_poke(x.sexp, 0, unsafe { r_char!("hi") });
+            r_chr_poke(x.sexp, 1, unsafe { r_char!("there") });
+            r_chr_poke(x.sexp, 2, r_str_na());
+            let label = argument_label(String::from("x"), x.sexp);
+            assert_eq!(label, String::from("x = c(\"hi\", \"there\", NA)"));
+        })
+    }
+
+    #[test]
+    fn test_argument_label_scalars() {
+        r_test(|| {
+            let x = RObject::from(r_alloc_logical(1));
+            r_lgl_poke(x.sexp, 0, 1);
+            let label = argument_label(String::from("x"), x.sexp);
+            assert_eq!(label, String::from("x = TRUE"));
+
+            let x = RObject::from(r_alloc_integer(1));
+            r_int_poke(x.sexp, 0, 1);
+            let label = argument_label(String::from("x"), x.sexp);
+            assert_eq!(label, String::from("x = 1L"));
+
+            let x = RObject::from(r_alloc_double(1));
+            r_dbl_poke(x.sexp, 0, 1.5);
+            let label = argument_label(String::from("x"), x.sexp);
+            assert_eq!(label, String::from("x = 1.5"));
+
+            let x = RObject::from(r_alloc_complex(1));
+            r_cpl_poke(x.sexp, 0, libr::Rcomplex { r: 1.5, i: 2.5 });
+            let label = argument_label(String::from("x"), x.sexp);
+            assert_eq!(label, String::from("x = 1.5+2.5i"));
+
+            let x = RObject::from(r_alloc_character(1));
+            r_chr_poke(x.sexp, 0, unsafe { r_char!("hi") });
+            let label = argument_label(String::from("x"), x.sexp);
+            assert_eq!(label, String::from("x = \"hi\""));
         })
     }
 }
