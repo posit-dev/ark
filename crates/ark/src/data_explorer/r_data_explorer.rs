4004ac737e23796f0415ee5c2e96ae901cb4920b
Daniel Falbel <dfalbel@gmail.com>
2024-08-02 13:56:27 -0300

    Apply suggestion from code review
    Co-authored-by: Davis Vaughan <davis@rstudio.com>

--- crates/ark/src/data_explorer/r_data_explorer.rs
+++ crates/ark/src/data_explorer/r_data_explorer.rs
@@ -382,10 +382,9 @@ impl RDataExplorer {
                 self.sorted_indices = Some(r_task(|| self.r_sort_rows())?);
             }
 
-            // This is a no-op if there are no filters to be applied.
+            // Recompute and apply filters and sorts.
             let (indices, _) = self.row_filters_compute()?;
             self.filtered_indices = indices;
-
             self.apply_sorts_and_filters();
 
             DataExplorerFrontendEvent::DataUpdate
