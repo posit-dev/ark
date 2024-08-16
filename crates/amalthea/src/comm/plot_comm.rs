977fb7036e17bcc3ddb0bc25b729399e80e13a6d
Wasim Lorgat <mwlorgat@gmail.com>
2024-08-16 15:34:25 +0200

    Placeholder implementation of intrinsic plot sizing (#470)

--- crates/amalthea/src/comm/plot_comm.rs
+++ crates/amalthea/src/comm/plot_comm.rs
@@ -11,6 +11,22 @@
 use serde::Deserialize;
 use serde::Serialize;
 
+/// The intrinsic size of a plot, if known
+#[derive(Clone, Debug, Serialize, Deserialize, PartialEq)]
+pub struct IntrinsicSize {
+	/// The width of the plot
+	pub width: f64,
+
+	/// The height of the plot
+	pub height: f64,
+
+	/// The unit of measurement of the plot's dimensions
+	pub unit: PlotUnit,
+
+	/// The source of the intrinsic size e.g. 'Matplotlib'
+	pub source: String
+}
+
 /// A rendered plot
 #[derive(Clone, Debug, Serialize, Deserialize, PartialEq)]
 pub struct PlotResult {
@@ -19,6 +35,16 @@ pub struct PlotResult {
 
 	/// The MIME type of the plot data
 	pub mime_type: String
+}
+
+/// The size of a plot
+#[derive(Clone, Debug, Serialize, Deserialize, PartialEq)]
+pub struct PlotSize {
+	/// The plot's height, in pixels
+	pub height: i64,
+
+	/// The plot's width, in pixels
+	pub width: i64
 }
 
 /// Possible values for Format in Render
@@ -41,14 +67,24 @@ pub enum RenderFormat {
 	Pdf
 }
 
+/// Possible values for PlotUnit
+#[derive(Clone, Debug, Serialize, Deserialize, PartialEq, strum_macros::Display)]
+pub enum PlotUnit {
+	#[serde(rename = "pixels")]
+	#[strum(to_string = "pixels")]
+	Pixels,
+
+	#[serde(rename = "inches")]
+	#[strum(to_string = "inches")]
+	Inches
+}
+
 /// Parameters for the Render method.
 #[derive(Clone, Debug, Serialize, Deserialize, PartialEq)]
 pub struct RenderParams {
-	/// The requested plot height, in pixels
-	pub height: i64,
-
-	/// The requested plot width, in pixels
-	pub width: i64,
+	/// The requested size of the plot. If not provided, the plot will be
+	/// rendered at its intrinsic size.
+	pub size: Option<PlotSize>,
 
 	/// The pixel ratio of the display device
 	pub pixel_ratio: f64,
@@ -63,10 +99,17 @@ pub struct RenderParams {
 #[derive(Clone, Debug, Serialize, Deserialize, PartialEq)]
 #[serde(tag = "method", content = "params")]
 pub enum PlotBackendRequest {
+	/// Get the intrinsic size of a plot, if known.
+	///
+	/// The intrinsic size of a plot is the size at which a plot would be if
+	/// no size constraints were applied by Positron.
+	#[serde(rename = "get_intrinsic_size")]
+	GetIntrinsicSize,
+
 	/// Render a plot
 	///
-	/// Requests a plot to be rendered at a given height and width. The plot
-	/// data is returned in a base64-encoded string.
+	/// Requests a plot to be rendered. The plot data is returned in a
+	/// base64-encoded string.
 	#[serde(rename = "render")]
 	Render(RenderParams),
 
@@ -78,6 +121,9 @@ pub enum PlotBackendRequest {
 #[derive(Clone, Debug, Serialize, Deserialize, PartialEq)]
 #[serde(tag = "method", content = "result")]
 pub enum PlotBackendReply {
+	/// The intrinsic size of a plot, if known
+	GetIntrinsicSizeReply(Option<IntrinsicSize>),
+
 	/// A rendered plot
 	RenderReply(PlotResult),
 
