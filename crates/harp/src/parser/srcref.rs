900d09414e218b59206bac35f315b4fa20a04c5e
Lionel Henry <lionel.hry@gmail.com>
2024-08-26 16:11:03 +0200

    Use `[ )` ranges for lines too

--- crates/harp/src/parser/srcref.rs
+++ crates/harp/src/parser/srcref.rs
@@ -64,13 +64,13 @@ impl TryFrom<RObject> for SrcRef {
 
         let line = std::ops::Range {
             start: (value.get_value(0)? - 1) as usize,
-            end: (value.get_value(2)? - 1) as usize,
+            end: (value.get_value(2)?) as usize,
         };
 
         let line_parsed = if unsafe { value.len() >= 8 } {
             std::ops::Range {
                 start: (value.get_value(6)? - 1) as usize,
-                end: (value.get_value(7)? - 1) as usize,
+                end: (value.get_value(7)?) as usize,
             }
         } else {
             line.clone()
@@ -129,20 +129,20 @@ mod tests {
             let utf8 = &srcrefs[1];
             let bar = &srcrefs[2];
 
-            assert_eq!(foo.line, Range { start: 0, end: 0 });
-            assert_eq!(foo.line_virtual, Range { start: 0, end: 0 });
+            assert_eq!(foo.line, Range { start: 0, end: 1 });
+            assert_eq!(foo.line_virtual, Range { start: 0, end: 1 });
             assert_eq!(foo.column, Range { start: 0, end: 3 });
             assert_eq!(foo.column_byte, Range { start: 0, end: 3 });
 
             // `column_byte` is different because the character takes up two bytes
-            assert_eq!(utf8.line, Range { start: 2, end: 2 });
-            assert_eq!(utf8.line_virtual, Range { start: 2, end: 2 });
+            assert_eq!(utf8.line, Range { start: 2, end: 3 });
+            assert_eq!(utf8.line_virtual, Range { start: 2, end: 3 });
             assert_eq!(utf8.column, Range { start: 0, end: 1 });
             assert_eq!(utf8.column_byte, Range { start: 0, end: 2 });
 
             // Ends on different lines
-            assert_eq!(bar.line, Range { start: 3, end: 5 });
-            assert_eq!(bar.line_virtual, Range { start: 3, end: 5 });
+            assert_eq!(bar.line, Range { start: 3, end: 6 });
+            assert_eq!(bar.line_virtual, Range { start: 3, end: 6 });
             assert_eq!(bar.column, Range { start: 0, end: 1 });
             assert_eq!(bar.column_byte, Range { start: 0, end: 1 });
         })
@@ -156,12 +156,12 @@ mod tests {
             let foo = &srcrefs[0];
             let bar = &srcrefs[1];
 
-            assert_eq!(foo.line, Range { start: 0, end: 0 });
-            assert_eq!(foo.line_virtual, Range { start: 0, end: 0 });
+            assert_eq!(foo.line, Range { start: 0, end: 1 });
+            assert_eq!(foo.line_virtual, Range { start: 0, end: 1 });
 
             // Custom line via directive
-            assert_eq!(bar.line, Range { start: 2, end: 2 });
-            assert_eq!(bar.line_virtual, Range { start: 4, end: 4 });
+            assert_eq!(bar.line, Range { start: 2, end: 3 });
+            assert_eq!(bar.line_virtual, Range { start: 4, end: 5 });
         })
     }
 }
