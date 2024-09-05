42009d3af9617a0aca1fe0a73b435bc64fb31f87
Lionel Henry <lionel.hry@gmail.com>
2024-09-05 09:00:53 +0000

    Validate data frame columns

--- crates/harp/src/data_frame.rs
+++ crates/harp/src/data_frame.rs
@@ -2,6 +2,12 @@ use crate::vector::Vector;
 use crate::List;
 use crate::RObject;
 
+/// Typed data frame
+///
+/// Type guarantees:
+/// - Storage type is list.
+/// - Class is `"data.frame"`.
+/// - All columns are vectors and the same size as the data frame.
 #[derive(Debug)]
 pub struct DataFrame {
     pub list: List,
@@ -33,6 +39,23 @@ impl DataFrame {
             ));
         };
 
+        // Validate columns
+        for obj in list.iter() {
+            let obj = RObject::view(obj);
+
+            if unsafe { libr::Rf_isVector(obj.sexp) == 0 } {
+                return Err(harp::unexpected_structure!(
+                    "Data frame column must be a vector"
+                ));
+            }
+
+            if obj.length() as usize != nrow {
+                return Err(harp::unexpected_structure!(
+                    "Data frame column must be the same size as the number of rows"
+                ));
+            }
+        }
+
         Ok(Self {
             list,
             obj,
@@ -46,8 +69,12 @@ impl DataFrame {
 #[cfg(test)]
 mod tests {
     use crate::assert_match;
+    use crate::r_alloc_integer;
     use crate::r_chr_poke;
+    use crate::r_list_poke;
     use crate::test::r_test;
+    use crate::vector::IntegerVector;
+    use crate::vector::Vector;
     use crate::DataFrame;
     use crate::List;
     use crate::RObject;
@@ -94,6 +121,37 @@ mod tests {
             assert_match!(df, harp::Result::Err(err) => {
                 assert_match!(err, harp::Error::UnexpectedStructure(..));
                 assert!(format!("{err}").contains("missing names"))
+            });
+        })
+    }
+
+    #[test]
+    fn test_data_frame_bad_column_type() {
+        r_test(|| {
+            let df = harp::parse_eval_base("data.frame(x = 1:2, y = 3:4)").unwrap();
+            r_list_poke(df.sexp, 0, RObject::null().sexp);
+
+            let df = DataFrame::new(df.sexp);
+
+            assert_match!(df, harp::Result::Err(err) => {
+                assert_match!(err, harp::Error::UnexpectedStructure(..));
+                assert!(format!("{err}").contains("must be a vector"))
+            });
+        })
+    }
+
+    #[test]
+    fn test_data_frame_bad_column_size() {
+        r_test(|| {
+            let df = harp::parse_eval_base("data.frame(x = 1:2, y = 3:4)").unwrap();
+            let bad_col = r_alloc_integer(3);
+            r_list_poke(df.sexp, 0, bad_col);
+
+            let df = DataFrame::new(df.sexp);
+
+            assert_match!(df, harp::Result::Err(err) => {
+                assert_match!(err, harp::Error::UnexpectedStructure(..));
+                assert!(format!("{err}").contains("must be the same size"))
             });
         })
     }
