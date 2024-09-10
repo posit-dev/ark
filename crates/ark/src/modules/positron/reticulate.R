607b252a21b0075d02456b3d0c96e45689da10ae
Daniel Falbel <dfalbel@gmail.com>
2024-09-10 14:43:34 -0300

    Create a method to start kernels

--- crates/ark/src/modules/positron/reticulate.R
+++ crates/ark/src/modules/positron/reticulate.R
@@ -9,3 +9,25 @@
 .ps.reticulate_open <- function(input="") {
     .ps.Call("ps_reticulate_open", input)
 }
+
+#' @export
+.ps.rpc.reticulate_start_kernel <- function(kernelPath, connectionFile, logFile, logLevel) {
+    # We execute as interactive to allow reticulate to prompt for installation
+    # of required environments and/or Python packages.
+    local_options(interactive = TRUE)
+    tryCatch({
+        reticulate:::py_run_file_on_thread(
+            file = kernelPath,
+            args = c(
+                "-f", connectionFile,
+                "--logfile", logFile,
+                "--loglevel", logLevel,
+                "--session-mode", "console"
+            )
+        )
+        # Empty string means that no error happened.
+        ""
+    }, error = function(err) {
+        conditionMessage(err)
+    })
+}
