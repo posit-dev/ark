05a478e0be5ddd6498a34a9b751ebd0ad838cecf
Daniel Falbel <dfalbel@gmail.com>
2024-09-13 21:52:16 -0300

    Add methods registration and dispatching

---
+++ crates/ark/src/variables/methods.rs
@@ -0,0 +1,95 @@
+//
+// methods.rs
+//
+// Copyright (C) 2024 by Posit Software, PBC
+//
+//
+
+use std::collections::HashMap;
+use std::sync::RwLock;
+
+use anyhow::anyhow;
+use harp::environment::r_ns_env;
+use harp::environment::R_ENVS;
+use harp::exec::RFunction;
+use harp::exec::RFunctionExt;
+use harp::utils::r_classes;
+use harp::RObject;
+use libr::SEXP;
+use once_cell::sync::Lazy;
+
+pub static ARK_VARIABLES_METHODS: Lazy<RwLock<HashMap<String, HashMap<String, String>>>> =
+    Lazy::new(|| RwLock::new(HashMap::new()));
+
+fn register_variables_method(method: String, pkg: String, class: String) {
+    let mut tables = ARK_VARIABLES_METHODS.write().unwrap();
+
+    tables
+        .entry(method)
+        .or_insert_with(HashMap::new)
+        .insert(class, pkg);
+}
+
+pub fn populate_variable_methods_table(pkg: String) -> anyhow::Result<()> {
+    let ns = r_ns_env(&pkg)?;
+    let symbol_names = ns.iter().filter_map(|f| match f {
+        Ok(binding) => {
+            let nm: String = binding.name.into();
+            Some(nm)
+        },
+        Err(_) => None,
+    });
+
+    let methods = vec!["ark_variable_display_value"];
+
+    for nm in symbol_names {
+        for method in methods.clone() {
+            if nm.starts_with(method) {
+                register_variables_method(
+                    String::from(method),
+                    pkg.clone(),
+                    nm.trim_start_matches(method).to_string(),
+                );
+                break;
+            }
+        }
+    }
+
+    Ok(())
+}
+
+pub fn dispatch_variables_method(method: String, x: SEXP) -> Option<String> {
+    // If the object doesn't have classes, just return None
+    let classes: harp::vector::CharacterVector = r_classes(x)?;
+
+    // Get the method table, if there isn't one return an empty string
+    let tables = ARK_VARIABLES_METHODS.read().unwrap();
+    let method_table = tables.get(&method)?;
+
+    for class in classes.iter().filter_map(|x| x) {
+        if let Some(pkg) = method_table.get(&class) {
+            return match call_method(method.clone(), pkg.clone(), class.clone(), x) {
+                Err(err) => {
+                    log::warn!("Failed dispatching `{pkg}::{method}.{class}`: {err}");
+                    continue; // Try the method for the next class if there's any
+                },
+                Ok(value) => value,
+            };
+        }
+    }
+    None
+}
+
+fn call_method<T>(method: String, pkg: String, class: String, x: SEXP) -> anyhow::Result<T>
+where
+    T: TryFrom<RObject>,
+{
+    let result = RFunction::new(pkg.as_str(), format!("{method}.{class}").as_str())
+        .add(x)
+        .call_in(R_ENVS.global)?;
+
+    match result.try_into() {
+        Err(_) => Err(anyhow!("Failed converting to method return type.")),
+        Ok(value) => Ok(value),
+    }
+}
