072c4dde9be3c6607238e440b47e72f847221a7b
Daniel Falbel <dfalbel@gmail.com>
2024-09-14 11:05:41 -0300

    add more supported methods

--- crates/ark/src/variables/methods.rs
+++ crates/ark/src/variables/methods.rs
@@ -10,6 +10,7 @@ use std::sync::RwLock;
 
 use anyhow::anyhow;
 use harp::environment::r_ns_env;
+use harp::environment::BindingValue;
 use harp::environment::R_ENVS;
 use harp::exec::RFunction;
 use harp::exec::RFunctionExt;
@@ -24,7 +25,7 @@ static ARK_VARIABLES_METHODS: Lazy<RwLock<HashMap<String, HashMap<String, String
 
 fn register_variables_method(method: String, pkg: String, class: String) {
     let mut tables = ARK_VARIABLES_METHODS.write().unwrap();
-
+    log::info!("Found method:{method} for class:{class} on pkg:{pkg}");
     tables
         .entry(method)
         .or_insert_with(HashMap::new)
@@ -44,15 +45,22 @@ pub fn populate_methods_from_loaded_namespaces() -> anyhow::Result<()> {
 
 pub fn populate_variable_methods_table(pkg: String) -> anyhow::Result<()> {
     let ns = r_ns_env(&pkg)?;
-    let symbol_names = ns.iter().filter_map(|f| match f {
-        Ok(binding) => {
-            let nm: String = binding.name.into();
-            Some(nm)
-        },
-        Err(_) => None,
-    });
+    let symbol_names = ns
+        .iter()
+        .filter_map(Result::ok)
+        .filter(|b| match b.value {
+            BindingValue::Standard { .. } => true,
+            BindingValue::Promise { .. } => true,
+            _ => false,
+        })
+        .map(|b| -> String { b.name.into() });
 
-    let methods = vec!["ark_variable_display_value"];
+    let methods = vec![
+        "ark_variable_display_value",
+        "ark_variable_display_type",
+        "ark_variable_has_children",
+        "ark_variable_kind",
+    ];
 
     for nm in symbol_names {
         for method in methods.clone() {
@@ -61,7 +69,8 @@ pub fn populate_variable_methods_table(pkg: String) -> anyhow::Result<()> {
                     String::from(method),
                     pkg.clone(),
                     // 1.. is used to remove the `.` that follows the method name
-                    nm.trim_start_matches(method)[1..].to_string(),
+                    nm.trim_start_matches(format!("{method}.").as_str())
+                        .to_string(),
                 );
                 break;
             }
@@ -75,6 +84,17 @@ pub fn dispatch_variables_method<T>(method: String, x: SEXP) -> Option<T>
 where
     T: TryFrom<RObject>,
 {
+    dispatch_variables_method_with_args(method, x, HashMap::new())
+}
+
+pub fn dispatch_variables_method_with_args<T>(
+    method: String,
+    x: SEXP,
+    args: HashMap<String, RObject>,
+) -> Option<T>
+where
+    T: TryFrom<RObject>,
+{
     // If the object doesn't have classes, just return None
     let classes: harp::vector::CharacterVector = r_classes(x)?;
 
@@ -84,7 +104,7 @@ where
 
     for class in classes.iter().filter_map(|x| x) {
         if let Some(pkg) = method_table.get(&class) {
-            return match call_method(method.clone(), pkg.clone(), class.clone(), x) {
+            return match call_method(method.clone(), pkg.clone(), class.clone(), x, args.clone()) {
                 Err(err) => {
                     log::warn!("Failed dispatching `{pkg}::{method}.{class}`: {err}");
                     continue; // Try the method for the next class if there's any
@@ -96,13 +116,24 @@ where
     None
 }
 
-fn call_method<T>(method: String, pkg: String, class: String, x: SEXP) -> anyhow::Result<T>
+fn call_method<T>(
+    method: String,
+    pkg: String,
+    class: String,
+    x: SEXP,
+    args: HashMap<String, RObject>,
+) -> anyhow::Result<T>
 where
     T: TryFrom<RObject>,
 {
-    let result = RFunction::new(pkg.as_str(), format!("{method}.{class}").as_str())
-        .add(x)
-        .call_in(R_ENVS.global)?;
+    let mut call = RFunction::new_internal(pkg.as_str(), format!("{method}.{class}").as_str());
+    call.add(x);
+
+    for (name, value) in args.into_iter() {
+        call.param(name.as_str(), value);
+    }
+
+    let result = call.call_in(R_ENVS.global)?;
 
     match result.try_into() {
         Err(_) => Err(anyhow!("Failed converting to method return type.")),
