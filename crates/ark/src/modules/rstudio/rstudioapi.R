b47b7353757cb5413dc90bb001f15c07664dea6d
Julia Silge <julia.silge@gmail.com>
2024-09-17 16:59:51 -0600

    Update `.rs.api.versionInfo()`

--- crates/ark/src/modules/rstudio/rstudioapi.R
+++ crates/ark/src/modules/rstudio/rstudioapi.R
@@ -1,20 +1,34 @@
 #' @export
 .rs.api.versionInfo <- function() {
-    # comments are what rstudioapi actually does
-    # https://github.com/rstudio/rstudio/blob/bb729e14867f6f95e26600d8b38e4551402cf7de/src/cpp/r/R/Api.R#L135-L145
-    # info <- list()
+
+    current_year <- format(Sys.Date(), "%Y")
+    positron_citation <-
+        utils::bibentry(
+            "Manual",
+            title = "Positron: A next generation data science IDE",
+            author = utils::person("Posit team"),
+            organization = "Posit Software, PBC",
+            address      = "Boston, MA",
+            year         = current_year,
+            url          = "http://www.posit.co/",
+            textVersion =
+                paste(
+                    paste0("Posit team (", current_year, "). "),
+                    "Positron: A next generation data science IDE. ",
+                    "Posit Software, PBC, Boston, MA. ",
+                    "URL http://www.posit.co/.",
+                    sep = ""
+                ),
+            mheader = "To cite Positron in publications use:",
+            mfooter = ""
+        )
+    class(positron_citation) <- c("citation", "bibentry")
+
     list(
-        # info$citation <- .Call("rs_rstudioCitation", PACKAGE = "(embedding)")
-        citation = NULL,
-        # info$mode <- .Call("rs_rstudioProgramMode", PACKAGE = "(embedding)")
-        mode = "desktop",
-        # info$edition <- .Call("rs_rstudioEdition", PACKAGE = "(embedding)")
-        # info$version <- .Call("rs_rstudioVersion", PACKAGE = "(embedding)")
-        # info$version <- package_version(info$version)
-        version = package_version("2023.3"),
-        # info$long_version <- .Call("rs_rstudioLongVersion", PACKAGE = "(embedding)")
-        long_version = "2023.03.0"
-        # info$release_name <- .Call("rs_rstudioReleaseName", PACKAGE = "(embedding)")
+        citation = positron_citation,
+        mode = Sys.getenv("POSITRON_MODE"),
+        version = package_version(Sys.getenv("POSITRON_VERSION")),
+        long_version = Sys.getenv("POSITRON_LONG_VERSION"),
+        ark_version = .ps.ark.version()
     )
-    #     info
 }
