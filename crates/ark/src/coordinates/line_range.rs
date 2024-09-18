a1a930913cb5c2e9572a6f55d8f698031df80d80
Lionel Henry <lionel.hry@gmail.com>
2024-09-18 08:18:49 +0200

    Don't wrap `std::ops::Range`

--- crates/ark/src/coordinates/line_range.rs
+++ crates/ark/src/coordinates/line_range.rs
@@ -1,33 +1,38 @@
 use std::cmp;
 
+/// Range over lines of text
+///
+/// The API is (incompletely) modeled after `text_size::TextRange` but the inner
+/// types are not as opaque so it's easy to work with line numbers coming from
+/// external sources. The type name mentions "line" to be self-documenting about
+/// what the range represents.
 #[derive(Clone, Debug, PartialEq, Eq, Hash)]
 pub struct LineRange {
-    pub inner: std::ops::Range<u32>,
+    start: u32,
+    end: u32,
 }
 
 /// Mirrors `text_size::TextRange` API (incompletely)
 impl LineRange {
     pub fn new(start: u32, end: u32) -> Self {
-        Self {
-            inner: std::ops::Range { start, end },
-        }
+        Self { start, end }
     }
 
     pub fn start(&self) -> u32 {
-        self.inner.start
+        self.start
     }
 
     pub fn end(&self) -> u32 {
-        self.inner.end
+        self.end
     }
 
     pub fn contains(&self, line: u32) -> bool {
-        self.inner.contains(&line)
+        self.start <= line && line < self.end
     }
 
     pub fn cover(&self, other: Self) -> Self {
-        let start = cmp::min(self.start(), other.start());
-        let end = cmp::max(self.end(), other.end());
+        let start = cmp::min(self.start, other.start);
+        let end = cmp::max(self.end, other.end);
         Self::new(start, end)
     }
 }
