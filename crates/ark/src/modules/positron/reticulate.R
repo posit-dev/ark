49c9a99b29283179a10648c70f80296ceb0668de
Daniel Falbel <dfalbel@gmail.com>
2024-09-20 15:08:41 -0300

    Add more method to make checks on R environment

--- crates/ark/src/modules/positron/reticulate.R
+++ crates/ark/src/modules/positron/reticulate.R
@@ -1,16 +1,10 @@
-
-#' @export
-.ps.rpc.reticulate_interpreter_path <- function() {
-    if (!.ps.is_installed("reticulate")) return("")
-    reticulate::py_discover_config()$python
-}
-
 #' @export
 .ps.reticulate_open <- function(input="") {
     .ps.Call("ps_reticulate_open", input)
 }
 
 #' Used by the front-end to install reticulate
+#' A modal asking to install is shown before calling this.
 #' @export
 .ps.rpc.install_reticulate <- function() {
     tryCatch({
@@ -21,11 +15,72 @@
     })
 }
 
+#' Called by the front-end right before starting the reticulate session.
+#'
+#' At this point it should be fine to load Python if it's not loaded, and
+#' check if it can be started and if necessary packages are installed.
+#' @export
+.ps.rpc.reticulate_check_prerequisites <- function() {
+
+    # This should return a list with the following fields:
+    # python: NULL or string
+    # venv: NULL or string
+    # ipykernel: NULL or string
+    # error: NULL or string
+
+    config <- tryCatch({
+        reticulate::py_discover_config()
+    }, error = function(err) {
+        err
+    })
+
+    if (inherits(config, "error")) {
+        # py_discover_config() can fail if the user forced a Python session
+        # via RETICULATE_PYTHON, but this version doesn't exist.
+        return(list(error = conditionMessage(config)))
+    }
+
+    if (is.null(config) || is.null(config$python)) {
+        # The front-end will offer to install Python.
+        return(list(python = NULL, error = NULL))
+    }
+
+    python <- config$python
+    venv <- config$virtualenv
+
+    # Check that python can be loaded, if it can't we will throw
+    # an error. which is unrecoverable.
+    config <- tryCatch({
+        reticulate::py_config()
+    }, error = function(err) {
+        err
+    })
+
+    if (inherits(config, "error")) {
+        return(list(python = python, venv = venv, error = conditionMessage(config)))
+    }
+
+    # Now check ipykernel
+    ipykernel <- tryCatch({
+        reticulate::py_module_available("ipykernel")
+    }, error = function(err) {
+        err
+    })
+
+    if (inherits(ipykernel, "error")) {
+        return(list(python = python, venv = venv, error = as.character(ipykernel)))
+    }
+
+    list(
+        python = config$python,
+        venv = venv,
+        ipykernel = ipykernel,
+        error = NULL
+    )
+}
+
 #' @export
 .ps.rpc.reticulate_start_kernel <- function(kernelPath, connectionFile, logFile, logLevel) {
-    # We execute as interactive to allow reticulate to prompt for installation
-    # of required environments and/or Python packages.
-    local_options(interactive = TRUE)
     tryCatch({
         reticulate:::py_run_file_on_thread(
             file = kernelPath,
