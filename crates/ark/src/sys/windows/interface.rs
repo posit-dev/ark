2ff80e800b60b1d8f28597c585af71e4f405b864
DavisVaughan <davis@posit.co>
2024-09-27 11:49:23 -0400

    Use `r_suicide()`

--- crates/ark/src/sys/windows/interface.rs
+++ crates/ark/src/sys/windows/interface.rs
@@ -25,6 +25,7 @@ use stdext::cargs;
 use crate::interface::r_busy;
 use crate::interface::r_read_console;
 use crate::interface::r_show_message;
+use crate::interface::r_suicide;
 use crate::interface::r_write_console;
 use crate::sys::windows::strings::system_to_utf8;
 
