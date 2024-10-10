3a4c4466f55ba402ee3ace9940ca555c05e55779
Davis Vaughan <davis@rstudio.com>
2024-10-10 13:46:29 -0400

    Always pass all zmq identities through the `Originator` (#574)

--- crates/amalthea/src/wire/originator.rs
+++ crates/amalthea/src/wire/originator.rs
@@ -10,14 +10,14 @@ use crate::wire::jupyter_message::JupyterMessage;
 
 #[derive(Debug, Clone)]
 pub struct Originator {
-    pub zmq_id: Vec<u8>,
+    pub zmq_identities: Vec<Vec<u8>>,
     pub header: JupyterHeader,
 }
 
 impl<T> From<&JupyterMessage<T>> for Originator {
     fn from(msg: &JupyterMessage<T>) -> Originator {
         Originator {
-            zmq_id: msg.zmq_identities[0].clone(),
+            zmq_identities: msg.zmq_identities.clone(),
             header: msg.header.clone(),
         }
     }
