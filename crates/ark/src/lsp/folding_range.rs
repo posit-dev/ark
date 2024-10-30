69404f7c917995e3d133db077a0cf318515d60f6
Dianyi Yang <kv9898@rubuky.com>
2024-10-30 15:25:35 -0400

    handle comments within functions

--- crates/ark/src/lsp/folding_range.rs
+++ crates/ark/src/lsp/folding_range.rs
@@ -13,12 +13,17 @@ pub fn folding_range(document: &Document) -> anyhow::Result<Vec<FoldingRange>> {
     let mut line_iter = text.lines().enumerate().peekable();
 
     let mut bracket_stack: Vec<(usize, usize)> = Vec::new(); // a stack of (start_line, start_character) tuples
-    let mut comment_stack: Vec<(usize, usize)> = Vec::new(); // a stack of (level, start_line) tuples
+    let mut comment_stack: Vec<Vec<(usize, usize)>> = vec![Vec::new()]; // a vector of stacks for each bracket level, within each stack is a vector of (level, start_line) tuples
 
     while let Some((line_idx, line)) = line_iter.next() {
         let line_text = line.to_string();
-        (folding_ranges, bracket_stack) =
-            bracket_processor(folding_ranges, bracket_stack, line_idx, &line_text);
+        (folding_ranges, bracket_stack, comment_stack) = bracket_processor(
+            folding_ranges,
+            bracket_stack,
+            comment_stack,
+            line_idx,
+            &line_text,
+        );
         (folding_ranges, comment_stack) =
             comment_processor(folding_ranges, comment_stack, line_idx, &line_text);
         // log_info!("line_idx: {:#?} line_text: {:#?}", line_idx, line_text);
@@ -35,9 +40,14 @@ pub fn folding_range(document: &Document) -> anyhow::Result<Vec<FoldingRange>> {
 fn bracket_processor(
     mut folding_ranges: Vec<FoldingRange>,
     mut bracket_stack: Vec<(usize, usize)>,
+    mut comment_stack: Vec<Vec<(usize, usize)>>,
     line_idx: usize,
     line_text: &str,
-) -> (Vec<FoldingRange>, Vec<(usize, usize)>) {
+) -> (
+    Vec<FoldingRange>,
+    Vec<(usize, usize)>,
+    Vec<Vec<(usize, usize)>>,
+) {
     // Remove any trailing comments (starting with #) and \n in line_text
     let line_text = line_text.split('#').next().unwrap_or("").trim_end();
     let mut whitespace_count = 0;
@@ -47,8 +57,11 @@ fn bracket_processor(
         match c {
             '{' => {
                 bracket_stack.push((line_idx, char_idx));
+                comment_stack.push(Vec::new());
             },
             '}' => {
+                (folding_ranges, comment_stack) =
+                    end_bracket_handler(folding_ranges, comment_stack, line_idx);
                 // If '}' is found, pop from the bracket_stack if it is not empty
                 if let Some((start_line, start_char)) = bracket_stack.pop() {
                     // Count the number of leading whitespace characters
@@ -75,7 +88,7 @@ fn bracket_processor(
         }
     }
 
-    (folding_ranges, bracket_stack)
+    (folding_ranges, bracket_stack, comment_stack)
 }
 
 fn bracket_range(
@@ -115,42 +128,68 @@ fn bracket_range(
     }
 }
 
+fn end_bracket_handler(
+    mut folding_ranges: Vec<FoldingRange>,
+    mut comment_stack: Vec<Vec<(usize, usize)>>,
+    line_idx: usize,
+) -> (Vec<FoldingRange>, Vec<Vec<(usize, usize)>>) {
+    // TODO: Iterate over the last elment of the comment stack and add it to the folding ranges by using the comment_range function
+    if let Some(last_section) = comment_stack.last() {
+        // Iterate over each (start level, start line) in the last section
+        for &(_level, start_line) in last_section.iter() {
+            // Add a new folding range for each range in the last section
+            folding_ranges.push(comment_range(start_line, line_idx - 1));
+        }
+    }
+
+    // Remove the last element from the comment stack after processing
+    comment_stack.pop();
+
+    (folding_ranges, comment_stack)
+}
+
 fn comment_processor(
     mut folding_ranges: Vec<FoldingRange>,
-    mut comment_stack: Vec<(usize, usize)>,
+    mut comment_stack: Vec<Vec<(usize, usize)>>,
     line_idx: usize,
     line_text: &str,
-) -> (Vec<FoldingRange>, Vec<(usize, usize)>) {
+) -> (Vec<FoldingRange>, Vec<Vec<(usize, usize)>>) {
     let Some((level, _title)) = parse_comment_as_section(line_text) else {
         return (folding_ranges, comment_stack); // return if the line is not a comment section
     };
 
+    if comment_stack.is_empty() {
+        log_error!("Folding Range: comment_stack should always contain at least one element here");
+        return (folding_ranges, comment_stack);
+    }
+
     loop {
-        if comment_stack.is_empty() {
-            comment_stack.push((level, line_idx));
+        if comment_stack.last().unwrap().is_empty() {
+            comment_stack.last_mut().unwrap().push((level, line_idx));
             return (folding_ranges, comment_stack); // return if the stack is empty
         }
 
-        let Some((last_level, _)) = comment_stack.last() else {
+        let Some((last_level, _)) = comment_stack.last().unwrap().last() else {
             log_error!("Folding Range: comment_stacks should not be empty here");
             return (folding_ranges, comment_stack);
         };
         if *last_level < level {
-            comment_stack.push((level, line_idx));
+            comment_stack.last_mut().unwrap().push((level, line_idx));
             break;
         } else if *last_level == level {
-            folding_ranges.push(comment_range(comment_stack.last().unwrap().1, line_idx - 1));
-
-            // Log a copy of folding_range
-            let folding_range_copy = folding_ranges.last().unwrap().clone();
-            log_info!("folding_range_copy: {:#?}", folding_range_copy);
-
-            comment_stack.pop();
-            comment_stack.push((level, line_idx));
+            folding_ranges.push(comment_range(
+                comment_stack.last().unwrap().last().unwrap().1,
+                line_idx - 1,
+            ));
+            comment_stack.last_mut().unwrap().pop();
+            comment_stack.last_mut().unwrap().push((level, line_idx));
             break;
         } else {
-            folding_ranges.push(comment_range(comment_stack.last().unwrap().1, line_idx - 1));
-            comment_stack.pop(); // TODO: Handle case where comment_stack is empty
+            folding_ranges.push(comment_range(
+                comment_stack.last().unwrap().last().unwrap().1,
+                line_idx - 1,
+            ));
+            comment_stack.last_mut().unwrap().pop(); // TODO: Handle case where comment_stack is empty
         }
     }
 
