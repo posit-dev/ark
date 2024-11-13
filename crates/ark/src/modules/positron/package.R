5458fdfb61be0526a875dc79c276ea61deee9863
Jenny Bryan <jenny.f.bryan@gmail.com>
2024-11-13 13:38:17 -0800

    Expose package version info

--- crates/ark/src/modules/positron/package.R
+++ crates/ark/src/modules/positron/package.R
@@ -21,6 +21,32 @@
 #' @export
 .ps.rpc.is_installed <- .ps.is_installed
 
+# Returns a list containing:
+#   * the version string if the package is installed and NULL otherwise
+#   * a minimum version to check for, with '0.0.0' as a dummy default
+#   * a logical indicating if package is installed at or above the minimum version
+#  This may seem weird, but it's impractical for positron-r to do version
+#  comparisons.
+#' @export
+.ps.rpc.packageVersion <- function(pkg, checkAgainst = "0.0.0") {
+    installed <- system.file(package = pkg) != ""
+
+    if (installed) {
+        version <- utils::packageVersion(pkg)
+        list(
+          version = as.character(version),
+          checkAgainst = as.character(checkAgainst),
+          checkResult = version >= checkAgainst
+        )
+    } else {
+        list(
+          version = NULL,
+          checkAgainst = as.character(checkAgainst),
+          checkResult = FALSE
+        )
+    }
+}
+
 #' @export
 .ps.rpc.install_packages <- function(packages) {
     for (pkg in packages) {
