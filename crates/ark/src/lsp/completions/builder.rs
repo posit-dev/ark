e4c69381d44fe62b73f00a621e08f68a2095fd0f
Jenny Bryan <jenny.f.bryan@gmail.com>
2025-03-10 14:01:39 -0700

    Move completions_from_unique_sources() into the builder

--- crates/ark/src/lsp/completions/builder.rs
+++ crates/ark/src/lsp/completions/builder.rs
@@ -3,8 +3,14 @@ use tower_lsp::lsp_types::CompletionItem;
 
 use crate::lsp::completions::parameter_hints::ParameterHints;
 use crate::lsp::completions::parameter_hints::{self};
-use crate::lsp::completions::sources::completions_from_composite_sources;
-use crate::lsp::completions::sources::completions_from_unique_sources;
+use crate::lsp::completions::sources::composite::completions_from_composite_sources;
+use crate::lsp::completions::sources::unique::colon::completions_from_single_colon;
+use crate::lsp::completions::sources::unique::comment::completions_from_comment;
+use crate::lsp::completions::sources::unique::custom::completions_from_custom_source;
+use crate::lsp::completions::sources::unique::extractor::completions_from_at;
+use crate::lsp::completions::sources::unique::extractor::completions_from_dollar;
+use crate::lsp::completions::sources::unique::namespace::completions_from_namespace;
+use crate::lsp::completions::sources::unique::string::completions_from_string;
 use crate::lsp::document_context::DocumentContext;
 use crate::lsp::state::WorldState;
 
@@ -26,10 +32,8 @@ impl<'a> CompletionBuilder<'a> {
     }
 
     pub fn build(self) -> Result<Vec<CompletionItem>> {
-        // Initially just delegate to existing functions
-        if let Some(completions) =
-            completions_from_unique_sources(self.context, self.parameter_hints)?
-        {
+        // Try unique sources first
+        if let Some(completions) = self.completions_from_unique_sources()? {
             return Ok(completions);
         }
 
@@ -37,5 +41,40 @@ impl<'a> CompletionBuilder<'a> {
         // set of reasonable completions based on loaded packages, the open
         // document, the current workspace, and any call related arguments
         completions_from_composite_sources(self.context, self.state, self.parameter_hints)
+    }
+
+    pub fn completions_from_unique_sources(&self) -> Result<Option<Vec<CompletionItem>>> {
+        // Try to detect a single colon first, which is a special case where we
+        // don't provide any completions
+        if let Some(completions) = completions_from_single_colon(self.context)? {
+            return Ok(Some(completions));
+        }
+
+        if let Some(completions) = completions_from_comment(self.context)? {
+            return Ok(Some(completions));
+        }
+
+        if let Some(completions) = completions_from_string(self.context)? {
+            return Ok(Some(completions));
+        }
+
+        if let Some(completions) = completions_from_namespace(self.context, self.parameter_hints)? {
+            return Ok(Some(completions));
+        }
+
+        if let Some(completions) = completions_from_custom_source(self.context)? {
+            return Ok(Some(completions));
+        }
+
+        if let Some(completions) = completions_from_dollar(self.context)? {
+            return Ok(Some(completions));
+        }
+
+        if let Some(completions) = completions_from_at(self.context)? {
+            return Ok(Some(completions));
+        }
+
+        // No unique sources of completions, allow composite sources to run
+        Ok(None)
     }
 }
--- crates/ark/src/lsp/completions/sources/unique.rs
+++ crates/ark/src/lsp/completions/sources/unique.rs
@@ -5,71 +5,11 @@
 //
 //
 
-mod colon;
-mod comment;
-mod custom;
-mod extractor;
+pub mod colon;
+pub mod comment;
+pub mod custom;
+pub mod extractor;
 mod file_path;
-mod namespace;
-mod string;
+pub mod namespace;
+pub mod string;
 mod subset;
-
-use anyhow::Result;
-use colon::completions_from_single_colon;
-use comment::completions_from_comment;
-use custom::completions_from_custom_source;
-use extractor::completions_from_at;
-use extractor::completions_from_dollar;
-use namespace::completions_from_namespace;
-use string::completions_from_string;
-use tower_lsp::lsp_types::CompletionItem;
-
-use crate::lsp::completions::parameter_hints::ParameterHints;
-use crate::lsp::document_context::DocumentContext;
-
-pub fn completions_from_unique_sources(
-    context: &DocumentContext,
-    parameter_hints: ParameterHints,
-) -> Result<Option<Vec<CompletionItem>>> {
-    log::info!("completions_from_unique_sources()");
-
-    // Try to detect a single colon first, which is a special case where we
-    // don't provide any completions
-    if let Some(completions) = completions_from_single_colon(context) {
-        return Ok(Some(completions));
-    }
-
-    // Try comment / roxygen2 completions
-    if let Some(completions) = completions_from_comment(context)? {
-        return Ok(Some(completions));
-    }
-
-    // Try string (like file path) completions
-    if let Some(completions) = completions_from_string(context)? {
-        return Ok(Some(completions));
-    }
-
-    // Try `package::prefix` (or `:::`) namespace completions
-    if let Some(completions) = completions_from_namespace(context, parameter_hints)? {
-        return Ok(Some(completions));
-    }
-
-    // Try specialized custom completions
-    // (Should be before more general ast / call completions)
-    if let Some(completions) = completions_from_custom_source(context)? {
-        return Ok(Some(completions));
-    }
-
-    // Try `$` completions
-    if let Some(completions) = completions_from_dollar(context)? {
-        return Ok(Some(completions));
-    }
-
-    // Try `@` completions
-    if let Some(completions) = completions_from_at(context)? {
-        return Ok(Some(completions));
-    }
-
-    // No unique sources of completions, allow composite sources to run
-    Ok(None)
-}
