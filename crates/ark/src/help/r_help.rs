43cb93ac16f40d302bac3d26bc5927debe61bc9b
Daniel Falbel <dfalbel@gmail.com>
2025-08-28 11:11:46 -0300

    Can simplify slightly

--- crates/ark/src/help/r_help.rs
+++ crates/ark/src/help/r_help.rs
@@ -250,11 +250,9 @@ impl RHelp {
         unsafe {
             let env = (|| {
                 #[cfg(not(test))]
-                {
-                    if let Some(debug_env) = &RMain::get().debug_env() {
-                        // Mem-Safety: Object protected by `RMain` for the duration of the `r_task()`
-                        return debug_env.sexp;
-                    }
+                if let Some(debug_env) = &RMain::get().debug_env() {
+                    // Mem-Safety: Object protected by `RMain` for the duration of the `r_task()`
+                    return debug_env.sexp;
                 }
                 R_GlobalEnv
             })();
