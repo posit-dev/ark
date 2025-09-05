d5931cf6af8d8d8002757e63c83cb7a7f6fecac0
Daniel Falbel <dfalbel@gmail.com>
2025-09-05 15:54:30 -0300

    delay registering until survival is loaded

--- crates/ark/src/modules/positron/methods.R
+++ crates/ark/src/modules/positron/methods.R
@@ -113,30 +113,3 @@ call_ark_method <- function(generic, object, ...) {
 
     NULL
 }
-
-# Register special methods for Surv objects.
-# Surv objects are matrices, but unlike matrices indexing using [
-# will index a row, and not an element of the matrix which is assumed
-# by the variables pane formatting code.
-.ark.register_method(
-    "ark_positron_variable_get_children",
-    "Surv",
-    function(x, width) {
-        list(
-            time = x[, 1],
-            status = x[, 2]
-        )
-    }
-)
-
-.ark.register_method(
-    "ark_positron_variable_get_child_at",
-    "Surv",
-    function(x, ..., index, name) {
-        if (!is.null(name)) {
-            x <- x[, name]
-        } else {
-            x <- x[, index]
-        }
-    }
-)
