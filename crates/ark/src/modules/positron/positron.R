0b46409ba5129cefad7dcc5efbd9c6e71450675c
Julia Silge <julia.silge@gmail.com>
2025-12-03 08:53:45 -0700

    Set `.Platform$GUI` just in the positron module

---
+++ crates/ark/src/modules/positron/positron.R
@@ -0,0 +1,10 @@
+#
+# positron.R
+#
+# Copyright (C) 2025 Posit Software, PBC. All rights reserved.
+#
+#
+
+.Platform <- base::.Platform
+.Platform$GUI <- "Positron"
+env_bind_force(baseenv(), ".Platform", .Platform)
