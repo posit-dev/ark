c19a5fa10405b7b314b1d05b1c9e4ca4b97b5530
Lionel Henry <lionel.hry@proton.me>
2026-01-28 16:14:51 +0100

    Add test for global sourcing

--- crates/ark/tests/kernel-hooks-source.rs
+++ crates/ark/tests/kernel-hooks-source.rs
@@ -37,22 +37,37 @@ fn test_source_local() {
 }
 
 #[test]
-fn test_source_local_fallback() {
+fn test_source_global() {
     let frontend = DummyArkFrontend::lock();
 
     let mut file = tempfile::NamedTempFile::new().unwrap();
-    write!(file, "foobar\n").unwrap();
+    write!(file, "foo\n").unwrap();
 
     let path = file.path().to_str().unwrap();
 
+    // Breakpoint injection path
+    frontend.execute_request_invisibly(r#"foo <- "worked!""#);
+
     let code = format!(
         r#"local({{
-    foobar <- "worked"
-    source("{path}", local = TRUE, encoding = "UTF-8")$value
+    foo <- "did not work!"
+    source("{path}")$value
 }})"#
     );
 
     frontend.execute_request(&code, |result| {
-        assert_eq!(result, "[1] \"worked\"");
+        assert_eq!(result, "[1] \"worked!\"");
+    });
+
+    // Fallback path (because we supply `encoding`)
+    let code = format!(
+        r#"local({{
+    foo <- "did not work!"
+    source("{path}", encoding = "UTF-8")$value
+}})"#
+    );
+
+    frontend.execute_request(&code, |result| {
+        assert_eq!(result, "[1] \"worked!\"");
     });
 }
