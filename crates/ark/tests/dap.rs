f97b33002dd702036854fe16d71b2335b0770d31
Lionel Henry <lionel.hry@proton.me>
2026-01-30 12:28:25 +0100

    Add simple browser test

--- crates/ark/tests/dap.rs
+++ crates/ark/tests/dap.rs
@@ -14,3 +14,15 @@ fn test_dap_initialize_and_disconnect() {
     // `start_dap()` connects and initializes, `Drop` disconnects
     let _dap = frontend.start_dap();
 }
+
+#[test]
+fn test_dap_stopped_at_browser() {
+    let frontend = DummyArkFrontend::lock();
+    let mut dap = frontend.start_dap();
+
+    frontend.debug_send_browser();
+    dap.recv_stopped();
+
+    frontend.debug_send_quit();
+    dap.recv_continued();
+}
