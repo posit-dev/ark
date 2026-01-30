7419686220107e51bbccae6ddd1e6761495b7134
Lionel Henry <lionel.hry@proton.me>
2026-01-30 14:06:17 +0100

    Test Threads and StackTrace

--- crates/ark/tests/dap.rs
+++ crates/ark/tests/dap.rs
@@ -6,13 +6,21 @@
 //
 
 use ark_test::DummyArkFrontend;
+use dap::types::StackFrame;
+use dap::types::Thread;
 
 #[test]
 fn test_dap_initialize_and_disconnect() {
     let frontend = DummyArkFrontend::lock();
 
     // `start_dap()` connects and initializes, `Drop` disconnects
-    let _dap = frontend.start_dap();
+    let mut dap = frontend.start_dap();
+
+    // First thing sent by frontend after connection
+    assert!(matches!(
+        dap.threads().as_slice(),
+        [Thread { id: -1, name }] if name == "R console"
+    ));
 }
 
 #[test]
@@ -21,7 +29,13 @@ fn test_dap_stopped_at_browser() {
     let mut dap = frontend.start_dap();
 
     frontend.debug_send_browser();
+
     dap.recv_stopped();
+
+    assert!(matches!(
+        dap.stack_trace().as_slice(),
+        [StackFrame { name, line: 1, .. }] if name == "<global>"
+    ));
 
     frontend.debug_send_quit();
     dap.recv_continued();
