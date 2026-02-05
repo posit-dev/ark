94bb4d4f216afef103ed30fb46ed5251d888432f
Lionel Henry <lionel.hry@proton.me>
2026-02-05 14:09:38 +0100

    Remove async API

--- crates/ark_test/src/lib.rs
+++ crates/ark_test/src/lib.rs
@@ -5,7 +5,6 @@
 //
 //
 
-pub mod accumulator;
 pub mod comm;
 pub mod dap_assert;
 pub mod dap_client;
@@ -13,7 +12,6 @@ pub mod dummy_frontend;
 pub mod tracing;
 
 // Re-export utilities from ark::fixtures for convenience
-pub use accumulator::*;
 pub use ark::fixtures::package_is_installed;
 pub use ark::fixtures::point_and_offset_from_cursor;
 pub use ark::fixtures::point_from_cursor;
