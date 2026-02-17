ba9010d1647e7f1666b08e733d87e8ebea68375d
Lionel Henry <lionel.hry@proton.me>
2026-02-17 14:03:35 +0100

    Safer handling of response channel

--- crates/ark/src/dap/dap_server.rs
+++ crates/ark/src/dap/dap_server.rs
@@ -125,7 +125,7 @@ pub fn start_dap(
         let (backend_events_tx, backend_events_rx) = unbounded::<DapBackendEvent>();
         let (responses_tx, responses_rx) = unbounded::<Response>();
         let (done_tx, done_rx) = bounded::<bool>(0);
-        state.lock().unwrap().responses_tx = Some(responses_tx);
+        server.responses_tx = Some(responses_tx);
         let output_clone = server.output.clone();
 
         // We need a scope to let the borrow checker know that
@@ -252,6 +252,7 @@ pub struct DapServer<R: Read, W: Write> {
     state: Arc<Mutex<Dap>>,
     r_request_tx: Sender<RRequest>,
     comm_tx: Option<CommOutgoingTx>,
+    responses_tx: Option<Sender<Response>>,
 }
 
 impl<R: Read, W: Write> DapServer<R, W> {
@@ -270,6 +271,7 @@ impl<R: Read, W: Write> DapServer<R, W> {
             state,
             r_request_tx,
             comm_tx: Some(comm_tx),
+            responses_tx: None,
         }
     }
 
@@ -715,6 +717,12 @@ impl<R: Read, W: Write> DapServer<R, W> {
         let frame_id = args.frame_id;
         let state = self.state.clone();
 
+        let Some(responses_tx) = self.responses_tx.clone() else {
+            let rsp = req.error("DAP: No responses channel for `evaluate`");
+            self.respond(rsp)?;
+            return Ok(());
+        };
+
         log::trace!("DAP: Spawning idle task for evaluate");
         spawn_idle_any_prompt(move |mut capture| async move {
             log::trace!("DAP: Idle task started for evaluate");
@@ -754,10 +762,7 @@ impl<R: Read, W: Write> DapServer<R, W> {
                 }
             };
 
-            let state = state.lock().unwrap();
-            if let Some(tx) = &state.responses_tx {
-                tx.send(rsp).log_err();
-            }
+            responses_tx.send(rsp).log_err();
         });
 
         Ok(())
