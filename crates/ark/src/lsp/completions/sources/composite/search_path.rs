d565fd42b93d075fcce44d94e400d2e31b55671d
Lionel Henry <lionel.hry@proton.me>
2026-02-20 08:47:53 +0100

    Don't drop environment protection

--- crates/ark/src/lsp/completions/sources/composite/search_path.rs
+++ crates/ark/src/lsp/completions/sources/composite/search_path.rs
@@ -1,7 +1,7 @@
 //
 // search_path.rs
 //
-// Copyright (C) 2023-2025 Posit Software, PBC. All rights reserved.
+// Copyright (C) 2023-2026 Posit Software, PBC. All rights reserved.
 //
 //
 
@@ -17,6 +17,7 @@ use libr::R_lsInternal;
 use libr::ENCLOS;
 use tower_lsp::lsp_types::CompletionItem;
 
+use crate::console;
 use crate::lsp::completions::completion_context::CompletionContext;
 use crate::lsp::completions::completion_item::completion_item_from_package;
 use crate::lsp::completions::completion_item::completion_item_from_symbol;
@@ -51,14 +52,14 @@ fn completions_from_search_path(
 
     unsafe {
         // Iterate through environments starting from the current frame environment.
-        let mut env = crate::console::eval_env().sexp;
+        let mut env = console::eval_env();
 
-        while env != R_EmptyEnv {
-            let is_pkg_env = r_env_is_pkg_env(env);
+        while env.sexp != R_EmptyEnv {
+            let is_pkg_env = r_env_is_pkg_env(env.sexp);
 
             // Get package environment name, if there is one
             let name = if is_pkg_env {
-                let name = RObject::from(r_pkg_env_name(env));
+                let name = RObject::from(r_pkg_env_name(env.sexp));
                 let name = String::try_from(name)?;
                 Some(name)
             } else {
@@ -77,7 +78,7 @@ fn completions_from_search_path(
             };
 
             // List symbols in the environment.
-            let symbols = R_lsInternal(env, 1);
+            let symbols = R_lsInternal(env.sexp, 1);
 
             // Create completion items for each.
             let vector = CharacterVector::new(symbols)?;
@@ -96,7 +97,7 @@ fn completions_from_search_path(
                 // Add the completion item.
                 match completion_item_from_symbol(
                     symbol,
-                    env,
+                    env.sexp,
                     name,
                     promise_strategy,
                     context.function_context()?,
@@ -111,7 +112,7 @@ fn completions_from_search_path(
             }
 
             // Get the next environment.
-            env = ENCLOS(env);
+            env.replace(ENCLOS(env.sexp));
         }
 
         // Include installed packages as well.
