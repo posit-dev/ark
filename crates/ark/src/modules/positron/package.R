6390a8df14a2bb4fb0991f29124837cacb7c2722
Julia Silge <julia.silge@gmail.com>
2026-02-26 19:47:20 -0700

    Add new functions to get Addins, R Markdown templates

--- crates/ark/src/modules/positron/package.R
+++ crates/ark/src/modules/positron/package.R
@@ -68,3 +68,100 @@
 .ps.rpc.get_attached_packages <- function(...) {
     .packages()
 }
+
+#' Get all installed RStudio addins
+#' @export
+.ps.rpc.getAddins <- function() {
+    pkgs <- .packages(all.available = TRUE)
+    out <- list()
+
+    for (pkg in pkgs) {
+        path <- system.file("rstudio", "addins.dcf", package = pkg)
+        if (!nzchar(path)) {
+            next
+        }
+
+        dcf <- tryCatch(read.dcf(path), error = function(e) NULL)
+        if (is.null(dcf)) {
+            next
+        }
+
+        cols <- colnames(dcf)
+
+        for (i in seq_len(nrow(dcf))) {
+            binding <- dcf_field(dcf, i, "Binding", cols)
+            if (!nzchar(binding)) {
+                next
+            }
+
+            out <- c(
+                out,
+                list(list(
+                    name = dcf_field(dcf, i, "Name", cols),
+                    description = dcf_field(dcf, i, "Description", cols),
+                    binding = binding,
+                    interactive = identical(
+                        tolower(dcf_field(dcf, i, "Interactive", cols, "true")),
+                        "true"
+                    ),
+                    package = pkg
+                ))
+            )
+        }
+    }
+
+    out
+}
+
+#' Get all installed R Markdown templates
+#' @export
+.ps.rpc.getRmdTemplates <- function() {
+    pkgs <- .packages(all.available = TRUE)
+    out <- list()
+
+    for (pkg in pkgs) {
+        path <- system.file("rmarkdown", "templates", package = pkg)
+        if (!nzchar(path)) {
+            next
+        }
+
+        template_dirs <- list.dirs(path, recursive = FALSE, full.names = TRUE)
+
+        for (dir in template_dirs) {
+            yaml_path <- file.path(dir, "template.yaml")
+            if (!file.exists(yaml_path)) {
+                next
+            }
+
+            meta <- tryCatch(yaml::read_yaml(yaml_path), error = function(e) {
+                NULL
+            })
+            if (is.null(meta)) {
+                next
+            }
+
+            out <- c(
+                out,
+                list(list(
+                    name = meta$name %||% basename(dir),
+                    description = meta$description %||% "",
+                    create_dir = isTRUE(meta$create_dir),
+                    package = pkg,
+                    template = basename(dir)
+                ))
+            )
+        }
+    }
+
+    out
+}
+
+# Safely get a field from a DCF matrix, returning default if missing or NA
+dcf_field <- function(dcf, row, field, cols, default = "") {
+    if (field %in% cols) {
+        value <- dcf[row, field]
+        if (is.na(value)) default else value
+    } else {
+        default
+    }
+}
