5a870433a78679b1cf454d33a0a85519c57caeb5
Lionel Henry <lionel.hry@proton.me>
2026-03-01 08:22:12 +0100

    Remove usage of `RThreadSafe` in data explorer comm

--- crates/ark/src/data_explorer/table.rs
+++ crates/ark/src/data_explorer/table.rs
@@ -1,7 +1,7 @@
 //
 // table.rs
 //
-// Copyright (C) 2024 by Posit Software, PBC
+// Copyright (C) 2024-2026 by Posit Software, PBC
 //
 //
 
@@ -11,34 +11,30 @@ use std::sync::Mutex;
 use anyhow::anyhow;
 use harp::RObject;
 
-use crate::thread::RThreadSafe;
-
 #[derive(Clone)]
 pub struct Table {
-    table: Arc<Mutex<Option<RThreadSafe<RObject>>>>,
+    table: Arc<Mutex<Option<RObject>>>,
 }
 
+// Safety: `Table` is only accessed from the R thread or from R idle tasks
+// (which also run on the R thread).
+unsafe impl Send for Table {}
+
 impl Table {
-    pub fn new(data: RThreadSafe<RObject>) -> Self {
+    pub fn new(data: RObject) -> Self {
         let table = Arc::new(Mutex::new(Some(data)));
         Self { table }
     }
 
-    // Get can only be called from the main thread as it will also call
-    // get in the RThreadSafe object.
-    // Get only result in errors when the table is no longer available, so this
-    // failing to get, can be used as a sign to cancel a task.
+    // Fails when the table has been deleted, which can be used as a signal
+    // to cancel a task.
     pub fn get(&self) -> anyhow::Result<RObject> {
         let guard = self.table.lock().unwrap();
-        let table = guard
-            .as_ref()
-            .ok_or(anyhow!("Table not found"))?
-            .get()
-            .clone();
+        let table = guard.as_ref().ok_or(anyhow!("Table not found"))?.clone();
         Ok(table)
     }
 
-    pub fn set(&mut self, data: RThreadSafe<RObject>) {
+    pub fn set(&mut self, data: RObject) {
         let mut table = self.table.lock().unwrap();
         *table = Some(data);
     }
