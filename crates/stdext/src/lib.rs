45ca94624f94b275550fab2349370b7dea111da7
Lionel Henry <lionel.hry@proton.me>
2026-03-12 12:04:43 +0100

    Allow `redundant_pattern_matching` in `assert_match!` macro

--- crates/stdext/src/lib.rs
+++ crates/stdext/src/lib.rs
@@ -52,7 +52,10 @@ macro_rules! assert_match {
     };
 
     ($expression:expr, $pattern:pat_param) => {
-        assert!(matches!($expression, $pattern))
+        #[allow(clippy::redundant_pattern_matching)]
+        {
+            assert!(matches!($expression, $pattern))
+        }
     };
 }
 
