fcb7dcf5d55b6c213ea73adc43c8a452ba855ac5
Thomas Lin Pedersen <thomasp85@gmail.com>
2026-05-05 09:16:48 +0200

    remove prefix and update symbol

--- crates/ark/src/lsp/symbols.rs
+++ crates/ark/src/lsp/symbols.rs
@@ -594,12 +594,12 @@ fn collect_call_test_that(
     }
 
     let name = string.node_as_str(&doc.contents)?;
-    let name = format!("Test: {name}");
+    let name = name.to_string();
 
     let start = doc.lsp_position_from_tree_sitter_point(node.start_position())?;
     let end = doc.lsp_position_from_tree_sitter_point(node.end_position())?;
 
-    let symbol = new_symbol_node(name, SymbolKind::FUNCTION, Range { start, end }, children);
+    let symbol = new_symbol_node(name, SymbolKind::TYPE_PARAMETER, Range { start, end }, children);
     symbols.push(symbol);
 
     Ok(())
