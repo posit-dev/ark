f00dc96280674d3b8d3d830c508a0a7cfd369d8b
Thomas Lin Pedersen <thomasp85@gmail.com>
2026-05-06 12:48:24 +0200

    Reformat

--- crates/ark/src/lsp/symbols.rs
+++ crates/ark/src/lsp/symbols.rs
@@ -599,12 +599,7 @@ fn collect_call_test_that(
     let start = doc.lsp_position_from_tree_sitter_point(node.start_position())?;
     let end = doc.lsp_position_from_tree_sitter_point(node.end_position())?;
 
-    let symbol = new_symbol_node(
-        name,
-        SymbolKind::FUNCTION,
-        Range { start, end },
-        children,
-    );
+    let symbol = new_symbol_node(name, SymbolKind::FUNCTION, Range { start, end }, children);
     symbols.push(symbol);
 
     Ok(())
