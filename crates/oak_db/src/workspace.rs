ea956265a5c8753ae775e61adc99162e7a4c0c92
Davis Vaughan <davis@posit.co>
2026-06-24 13:34:34 -0400

    Split `workspace_dependencies()` internals into smaller tracked queries

--- crates/oak_db/src/workspace.rs
+++ crates/oak_db/src/workspace.rs
@@ -7,22 +7,38 @@ use crate::RootKind;
 
 /// Packages the workspace depends on
 ///
-/// Sources:
-/// - `library()` / `require()`
-/// - `::` / `:::`
-/// - Workspace package `DESCRIPTION`s
-///
-/// Returned set of `Package`s is sorted and unique, maximizing backdating potential.
+/// Returned sorted on package name and unique, maximizing backdating potential.
 ///
 /// Unknown packages (not installed) are dropped, along with those that resolve to a
 /// workspace package (which is currently being worked on).
 #[salsa::tracked(returns(ref))]
 pub fn workspace_dependencies(db: &dyn Db) -> Vec<Package> {
-    // Maintains orderedness and uniqueness for us
+    // Split into several queries to maximize backdating (changing a workspace file
+    // shouldn't require re-querying `DESCRIPTION` dependencies)
+    let from_files = workspace_file_dependencies(db);
+    let from_packages = workspace_package_dependencies(db);
+
+    let mut packages: Vec<Package> = Vec::with_capacity(from_files.len() + from_packages.len());
+    packages.extend(from_files);
+    packages.extend(from_packages);
+
+    packages.sort_by_cached_key(|package| package.name(db));
+    packages.dedup_by_key(|package| package.name(db));
+
+    packages
+}
+
+/// Packages used by files / scripts in the workspace
+///
+/// i.e. `library()` / `require()` / `::` / `:::`
+///
+/// Returned sorted on package name and unique, maximizing backdating potential.
+#[salsa::tracked(returns(ref))]
+fn workspace_file_dependencies(db: &dyn Db) -> Vec<Package> {
+    // It's likely that we will have a lot of duplicated package use across workspace
+    // files, so we use a BTreeSet to avoid having to sort and dedup a large vector
     let mut names: BTreeSet<&str> = BTreeSet::new();
 
-    // Packages used by workspace scripts / files, i.e. `library()` or `require()` calls,
-    // and `::` or `:::` accesses
     for &file in workspace_files(db) {
         names.extend(
             file.used_packages(db)
@@ -31,10 +47,21 @@ pub fn workspace_dependencies(db: &dyn Db) -> Vec<Package> {
         );
     }
 
-    // Packages used by a workspace package's `DESCRIPTION` file
-    //
-    // We take `Depends` and `Imports`, but not `Suggests`, as `Suggests` will
-    // instead be referenced by `::` elsewhere as required
+    as_packages(names, db)
+}
+
+/// Packages used by a workspace package's `DESCRIPTION` file
+///
+/// We take `Depends` and `Imports`, but not `Suggests`, as `Suggests` will
+/// instead be referenced by `::` elsewhere as required
+///
+/// Returned sorted on package name and unique, maximizing backdating potential.
+#[salsa::tracked(returns(ref))]
+fn workspace_package_dependencies(db: &dyn Db) -> Vec<Package> {
+    // We aren't expecting there to be many duplicates here (probably none for single
+    // package workspaces), so a simple Vec is fine
+    let mut names = Vec::new();
+
     for root in db.workspace_roots().roots(db).iter() {
         for &package in root.packages(db) {
             if let Some(description) = package.description(db) {
@@ -44,9 +71,18 @@ pub fn workspace_dependencies(db: &dyn Db) -> Vec<Package> {
         }
     }
 
-    // Now look up the actual `Package` by name, throwing out ones that aren't actually
-    // installed, and packages that are actively being worked on in the workspace already
-    let mut packages = Vec::with_capacity(names.len());
+    names.sort();
+    names.dedup();
+
+    as_packages(names, db)
+}
+
+/// Converts an iterable of `names` into their corresponding `Package`s, throwing out:
+/// - Non-installed packages (untracked by the db)
+/// - Workspace packages (actively being worked on by the user, so `R/` is present
+///   already)
+fn as_packages<'a>(names: impl IntoIterator<Item = &'a str>, db: &dyn Db) -> Vec<Package> {
+    let mut packages = Vec::new();
 
     for name in names {
         let Some(package) = db.package_by_name(name) else {
