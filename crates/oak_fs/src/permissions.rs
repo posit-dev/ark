49976b15fe27006fdbfba908d24c6c29318f781f
Davis Vaughan <davis@posit.co>
2026-07-07 13:33:05 -0400

    Move `FileLock` into `oak_cache` and remove `oak_fs`

--- crates/oak_fs/src/permissions.rs
+++
@@ -1,8 +0,0 @@
-use std::path::Path;
-
-/// Mark a file as read only
-pub fn set_readonly(path: &Path) -> std::io::Result<()> {
-    let mut permissions = std::fs::metadata(path)?.permissions();
-    permissions.set_readonly(true);
-    std::fs::set_permissions(path, permissions)
-}
