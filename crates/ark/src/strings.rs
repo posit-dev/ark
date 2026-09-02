0ba749eaa37d9b855edb83290309adfc49673b9c
Jonathan McPherson <jonathan@posit.co>
2026-09-02 15:02:32 -0700

    strip carriage returns when splitting lines

--- crates/ark/src/strings.rs
+++ crates/ark/src/strings.rs
@@ -12,15 +12,8 @@
 /// Returns a `DoubleEndedIterator`, which is the same as the
 /// one returned by `split()` in this particular case.
 pub fn lines(text: &str) -> impl DoubleEndedIterator<Item = &str> {
-    text.split('\n').map(|line| {
-        let Some(line) = line.strip_suffix('\n') else {
-            return line;
-        };
-        let Some(line) = line.strip_suffix('\r') else {
-            return line;
-        };
-        line
-    })
+    text.split('\n')
+        .map(|line| line.strip_suffix('\r').unwrap_or(line))
 }
 
 #[cfg(test)]
@@ -31,5 +24,11 @@ mod tests {
     fn test_lines() {
         let lines: Vec<&str> = lines("foo\n\n\nbar\n\n").collect();
         assert_eq!(lines, vec!["foo", "", "", "bar", "", ""])
+    }
+
+    #[test]
+    fn test_lines_crlf() {
+        let lines: Vec<&str> = lines("foo\r\n\r\nbar\r\n").collect();
+        assert_eq!(lines, vec!["foo", "", "bar", ""])
     }
 }
